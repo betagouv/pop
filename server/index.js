1c14a88b5b798528bc955f9515ee699ab2d98df4
goffle <se.legoff@gmail.com>
2018-05-17 12:36:00 +0200

    add statics files

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ console.log('process.env :', process.env)
 //   preload: true
 // }));
 
-app.use(express.static(path.join(__dirname, '../build')));
+//app.use(express.static(path.join(__dirname, '../build')));
 
 app.route('*').all((req, res) => {
   res.sendFile(path.join(__dirname + './../build/index.html'));
