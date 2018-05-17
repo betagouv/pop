55d2ad4d4b51688acbc2d0c8a4f9018d65e1fd63
goffle <se.legoff@gmail.com>
2018-05-17 17:13:33 +0200

    pop

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@
 let es_url = 'http://localhost:9200/pop';
 
 if (process.env.prod) {
-  es_url = 'https://vpc-pop-j3zoezftjmyiagfgm76ckgu2xy.eu-west-3.es.amazonaws.com/pop'
+  es_url = 'https://vpc-pop-j3zoezftjmyiagfgm76ckgu2xy.eu-west-3.es.amazonaws.com/pop';
 }
 
 export {
