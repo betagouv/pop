6026e1c8246900ddf82268e2a52ec78bcc4fef51
goffle <se.legoff@gmail.com>
2018-05-17 17:25:23 +0200

    add prod env

--- src/config.js
+++ src/config.js
@@ -1,8 +1,9 @@
 //ES
+console.log('ENVIRONNEMENT PROD ', process.env.NODE_ENV)
 
 let es_url = 'http://localhost:9200/pop';
 
-if (process.env.prod) {
+if (process.env.NODE_ENV === 'production') {
   es_url = 'https://vpc-pop-j3zoezftjmyiagfgm76ckgu2xy.eu-west-3.es.amazonaws.com/pop';
 }
 
