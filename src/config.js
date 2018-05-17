f5a9370f3070b62d031b3246516f1b053af23b6c
goffle <se.legoff@gmail.com>
2018-05-17 21:00:58 +0200

    add indice

--- src/config.js
+++ src/config.js
@@ -1,19 +1,18 @@
 //ES
 
-let forceProd = true;
+let forceProd = false;
 
 console.log('FORCE PRODUCTION : ' + forceProd)
 
 let mongo_url = 'mongodb://127.0.0.1/pop';
 if (forceProd || process.env.NODE_ENV === 'production') {
-  mongo_url = 'mongodb+srv://goffle:neovidi75@cluster0-fkthi.mongodb.net/test';
+  mongo_url = 'mongodb+srv://goffle:neovidi75@cluster0-fkthi.mongodb.net/pop';
 }
 
-let es_url = '127.0.0.1:9200';
+let es_url = 'http://127.0.0.1:9200/pop';
 if (forceProd || process.env.NODE_ENV === 'production') {
-  es_url = 'https://search-pop-j3zoezftjmyiagfgm76ckgu2xy.eu-west-3.es.amazonaws.com';
+  es_url = 'https://search-pop-j3zoezftjmyiagfgm76ckgu2xy.eu-west-3.es.amazonaws.com/pop';
 }
-
 
 module.exports = {
   mongo_url,
