90dd90c5e9b905a6a111a7de93e1769c96644ab6
goffle <se.legoff@gmail.com>
2018-05-17 15:07:08 +0200

    pop

--- src/server/index.js
+++ src/server/index.js
@@ -18,7 +18,7 @@ console.log('START', new Date())
 app.use(express.static(path.join(__dirname, '/../../build')));
 
 app.route('*').all((req, res) => {
-  res.sendFile(path.join(__dirname, '/../../build/index.html')));
+  res.sendFile(path.join(__dirname, '/../../build/index.html'));
 });
 
 
