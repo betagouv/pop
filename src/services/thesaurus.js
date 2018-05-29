2c8d49869c68c338ab56cca83711521283ab04cb
goffle <se.legoff@gmail.com>
2018-05-29 18:46:52 +0200

    add config url on thesaurus

--- src/services/thesaurus.js
+++ src/services/thesaurus.js
@@ -1,8 +1,9 @@
+const { api_url } = require('../config.js');
 var X2JS = require('X2JS');
 var x2js = new X2JS();
 
 class Thesaurus {
-    getTermsBeginWithSomeStringByThesaurus(req, thesaurusId, preferredTermOnly = false, startIndex = 1, limit = 10) {
+    getTermsBeginWithSomeStringByThesaurus(req, thesaurusId, preferredTermOnly = false, startIndex = 0, limit = 10) {
         const body = `
         <soap:getTermsBeginWithSomeStringByThesaurus>
             <request>${req}</request>
@@ -13,21 +14,18 @@ class Thesaurus {
         </soap:getTermsBeginWithSomeStringByThesaurus>`
         return this._post(body, 'thesaurusTerm')
     }
-
     getPreferredTermByConceptId(thesaurusId) {
         const body = `<soap:getPreferredTermByConceptId>
                 <conceptId>${thesaurusId}</conceptId>
             </soap:getPreferredTermByConceptId>`
         return this._post(body, 'thesaurusConcept')
     }
-
     getTopConceptsByThesaurusId(thesaurusId) {
         const body = `<soap:getTopConceptsByThesaurusId>
                 <thesaurusId>${thesaurusId}</thesaurusId>
             </soap:getTopConceptsByThesaurusId>`
         return this._post(body, 'thesaurusConcept')
     }
-
     _post(req, service) {
         return new Promise((resolve, reject) => {
             let str = `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:soap="http://soap.ginco.mcc.fr/">
@@ -35,7 +33,7 @@ class Thesaurus {
             <soapenv:Body>${req}</soapenv:Body>
             </soapenv:Envelope>`
             var xmlhttp = new XMLHttpRequest();
-            xmlhttp.open('POST', `http://localhost:3000/thesaurus/${service}`, true);
+            xmlhttp.open('POST', `${api_url}/thesaurus/${service}`, true);
             xmlhttp.onreadystatechange = function () {
                 if (xmlhttp.readyState == 4) {
                     if (xmlhttp.status == 200) {
@@ -52,36 +50,6 @@ class Thesaurus {
             xmlhttp.send(str);
         })
     }
-
-    // _post(req, service) {
-
-
-
-
-    //     return new Promise((resolve, reject) => {
-    //         fetch(`http://localhost:3000/thesaurus/${service}`, {
-    //             body: envelopedBody, // must match 'Content-Type' header
-    //             headers: {
-    //                 'user-agent': 'POP',
-    //                 "Content-Type": "text/xml; charset=utf-8"
-    //             },
-    //             method: 'POST', 
-    //         }).then((response) => {
-    //             if (response.statusCode == 200) {
-    //                 try {
-
-    //                 } catch (e) {
-    //                     reject('Response malformed')
-    //                 }
-    //             } else {
-    //                 console.log('Error : ', response)
-    //                 reject('Err')
-    //             }
-    //         }).catch((err) => {
-    //             reject('Fetch Error :-S', err)
-    //         });
-    //     })
-    // }
 }
 
 const t = new Thesaurus();
