f5a4b0a9ae7c4c4e002582af4d700061e356a2b0
goffle <se.legoff@gmail.com>
2018-06-07 18:14:24 +0200

    read  env

--- src/config.js
+++ src/config.js
@@ -1,24 +1,8 @@
 //ES
 
-let forceProd = true;
-
-console.log('SEB ', process.env.PORT);
-console.log('ENV ', process);
-
-let mongo_url = 'mongodb://127.0.0.1/pop';
-if (forceProd || process.env.NODE_ENV === 'production') {
-    mongo_url = 'mongodb+srv://goffle:neovidi75@cluster0-fkthi.mongodb.net/pop';
-}
-
-let es_url = '127.0.0.1:9200';
-if (forceProd || process.env.NODE_ENV === 'production') {
-    es_url = 'https://search-pop-j3zoezftjmyiagfgm76ckgu2xy.eu-west-3.es.amazonaws.com';
-}
-
-let PORT = 3000;
-if (forceProd || process.env.NODE_ENV === 'production') {
-    PORT = 8081
-}
+const PORT = process.env.PORT || 3000;
+const mongo_url = process.env.DB_ENDPOINT || 'mongodb://127.0.0.1/pop';
+const es_url = process.env.ES_ENDPOINT || 'https://search-pop-j3zoezftjmyiagfgm76ckgu2xy.eu-west-3.es.amazonaws.com';
 
 module.exports = {
     mongo_url,
