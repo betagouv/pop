27a1d5af0a9d0a3b73c80af03c2e63612a0f499d
goffle <se.legoff@gmail.com>
2018-06-29 16:35:33 +0200

    be able to upload files

--- src/controllers/joconde.js
+++ src/controllers/joconde.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const AWS = require('aws-sdk');
 const router = express.Router();
 const multer = require('multer')
+const fs = require('fs');
 const upload = multer({ dest: 'uploads/' })
 const Joconde = require('../models/joconde');
+
+var s3 = new AWS.S3();
 
 
 router.post('/update', (req, res) => {
@@ -12,13 +16,56 @@ router.post('/update', (req, res) => {
     });
 })
 
-router.post('/create', upload.array(), (req, res) => {
-    console.log(req)
-    // res.sendStatus(200)
-    // Joconde.create(req.body).then((e) => {
-    //     res.sendStatus(200)
-    // });
+router.post('/create', upload.any(), (req, res) => {
+
+    const files = req.files || [];
+    const notice = JSON.parse(req.body.notice);
+
+    const arr = [];
+    for (var i = 0; i < req.files.length; i++) {
+        arr.push(uploadFile(`joconde/${notice.REF}/${req.files[i].originalname}`, req.files[i]))
+    }
+
+  
+    // console.log('NOTICE', notice);
+    arr.push(Joconde.create(notice))
+
+    Promise.all(arr)
+        .then(() => {
+            res.sendStatus(200)
+        })
+        .catch((e) => {
+            res.sendStatus(500)
+        })
 })
+
+
+
+
+function uploadFile(path, file) {
+    return new Promise((resolve, reject) => {
+        const data = fs.readFileSync(file.path);
+        const params = {
+            Bucket: "pop-phototeque",
+            Key: path,
+            Body: data,
+            ContentType: file.mimetype,
+            ACL: 'public-read'
+        };
+
+        s3.putObject(params, (err) => {
+            fs.unlinkSync(file.path);
+            if (err) {
+                console.log(err);
+                reject();
+            } else {
+                resolve()
+            }
+        });
+    })
+}
+
+
 
 router.get('/', (req, res) => {
     var ref = req.query.ref;
