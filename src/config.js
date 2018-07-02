5a3bc5edae0c788e4ee5ef5bf5162ad9e67e7f6e
goffle <se.legoff@gmail.com>
2018-07-02 11:09:42 +0200

    update config

--- src/config.js
+++ src/config.js
@@ -2,17 +2,21 @@
 
 let forceProd = false;
 
-console.log('ENV DETECTED : ',process.env.NODE_ENV)
+console.log('ENV DETECTED : ', process.env.NODE_ENV)
 
 let api_url = 'http://127.0.0.1:3000';
-if (forceProd || process.env.NODE_ENV === 'production') {
-  api_url = 'http://pop-api.eu-west-3.elasticbeanstalk.com';
-}
-
-
 let es_url = 'http://127.0.0.1:9200';
-if (forceProd || process.env.NODE_ENV === 'production') {
-  es_url = 'https://search-pop-j3zoezftjmyiagfgm76ckgu2xy.eu-west-3.es.amazonaws.com';
+
+switch (process.env.NODE_ENV) {
+  case 'production':
+    api_url = 'http://pop-api.eu-west-3.elasticbeanstalk.com';
+    es_url = 'https://search-pop-j3zoezftjmyiagfgm76ckgu2xy.eu-west-3.es.amazonaws.com';
+    break;
+
+  case 'staging':
+    api_url = 'http://pop-api-staging.eu-west-3.elasticbeanstalk.com';
+    es_url = 'https://search-pop-staging-zukwe7tuull7zntiuqs3mp3gr4.eu-west-3.es.amazonaws.com';
+    break;
 }
 
 module.exports = {
