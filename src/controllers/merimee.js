cded295988aecba070b1e4e494089cc5fbc57d8d
goffle <se.legoff@gmail.com>
2018-08-01 15:09:09 +0200

    update merimee

--- src/controllers/merimee.js
+++ src/controllers/merimee.js
@@ -1,23 +1,43 @@
 const express = require('express');
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
 const router = express.Router()
 const Merimee = require('../models/merimee');
+const { uploadFile } = require('./utils');
 
 
-router.post('/:ref', (req, res) => {
-    var ref = req.query.ref;
-    Merimee.findOneAndUpdate({ REF: ref }, req.body, { upsert: true }).then((e) => {
+router.put('/:ref', upload.any(), (req, res) => {
+    const ref = req.params.ref;
+    const notice = JSON.parse(req.body.notice);
+
+    //UPDATE MAJ DATE ( couldnt use hook ...)
+
+    const now = new Date();
+    const formattedNow = ("0" + now.getDate()).slice(-2) + '-' + ("0" + (now.getMonth() + 1)).slice(-2) + '-' + now.getFullYear();
+    notice.DMAJ = formattedNow;
+
+    const arr = [];
+    // for (var i = 0; i < req.files.length; i++) {
+    //     arr.push(uploadFile(`mnr/${notice.REF}/${req.files[i].originalname}`, req.files[i]))
+    // }
+    arr.push(Merimee.findOneAndUpdate({ REF: ref }, notice, { upsert: true, new: true }))
+
+    Promise.all(arr).then(() => {
+        res.sendStatus(200)
+    }).catch((e) => {
+        res.sendStatus(500)
+    })
+})
+
+router.post('/', upload.any(), (req, res) => {
+    const notice = JSON.parse(req.body.notice);
+    Merimee.create(notice).then((e) => {
         res.sendStatus(200)
     });
 })
 
-router.post('/create', (req, res) => {
-    Merimee.create(req.body).then((e) => {
-        res.sendStatus(200)
-    });
-})
-
-router.get('/', (req, res) => {
-    var ref = req.query.ref;
+router.get('/:ref', (req, res) => {
+    const ref = req.params.ref;
     Merimee.findOne({ REF: ref }, (err, notice) => {
         if (err) {
             console.log('ERR', err)
--- src/controllers/palissy.js
+++ src/controllers/palissy.js
@@ -1,24 +1,44 @@
 const express = require('express');
 const router = express.Router()
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
 const Palissy = require('../models/palissy');
+const { uploadFile } = require('./utils');
 
 
-router.post('/update', (req, res) => {
-    var ref = req.query.ref;
-    Palissy.findOneAndUpdate({ REF: ref }, req.body, { upsert: true }).then((e) => {
+
+router.put('/:ref', upload.any(), (req, res) => {
+    const ref = req.params.ref;
+    const notice = JSON.parse(req.body.notice);
+
+    //UPDATE MAJ DATE ( couldnt use hook ...)
+
+    const now = new Date();
+    const formattedNow = ("0" + now.getDate()).slice(-2) + '-' + ("0" + (now.getMonth() + 1)).slice(-2) + '-' + now.getFullYear();
+    notice.DMAJ = formattedNow;
+
+    const arr = [];
+    // for (var i = 0; i < req.files.length; i++) {
+    //     arr.push(uploadFile(`mnr/${notice.REF}/${req.files[i].originalname}`, req.files[i]))
+    // }
+    arr.push(Palissy.findOneAndUpdate({ REF: ref }, notice, { upsert: true, new: true }))
+
+    Promise.all(arr).then(() => {
+        res.sendStatus(200)
+    }).catch((e) => {
+        res.sendStatus(500)
+    })
+})
+
+router.post('/', upload.any(), (req, res) => {
+    const notice = JSON.parse(req.body.notice);
+    Palissy.create(notice).then((e) => {
         res.sendStatus(200)
     });
 })
 
-router.post('/create', (req, res) => {
-    Palissy.create(req.body).then((e) => {
-        res.sendStatus(200)
-    });
-})
-
-router.get('/', (req, res) => {
-    var ref = req.query.ref;
-    console.log('GET PALISSY ', ref)
+router.get('/:ref', (req, res) => {
+    const ref = req.params.ref;
     Palissy.findOne({ REF: ref }, (err, notice) => {
         if (err) {
             console.log('ERR', err)
@@ -29,6 +49,10 @@ router.get('/', (req, res) => {
 })
 
 module.exports = router
+
+
+
+
 
 
 
