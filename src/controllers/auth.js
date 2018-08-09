f4e25fbd98f21a243e4dfd82a24558aea138dec4
goffle <se.legoff@gmail.com>
2018-08-09 18:13:22 +0200

    update password on API works

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -15,8 +15,6 @@ router.use(bodyParser.urlencoded({ extended: false }))
 router.use(bodyParser.json())
 
 router.post('/signup', (req, res) => {
-
-  console.log("GOT IT", req.body)
   if (!req.body.email || !req.body.group || !req.body.role || !req.body.institution) {
     return res.status(400).json({
       success: false,
@@ -27,6 +25,8 @@ router.post('/signup', (req, res) => {
   var password = generator.generate({ length: 10, numbers: true });
 
   const newUser = new User({
+    nom: req.body.nom,
+    prenom: req.body.prenom,
     email: req.body.email,
     group: req.body.group,
     role: req.body.role,
@@ -39,10 +39,9 @@ router.post('/signup', (req, res) => {
     if (err) {
       return res.status(400).json({
         success: false,
-        msg: 'Username already exists.'
+        msg: `L'utilisateur ${req.body.email} existe déja`
       })
     }
-
 
     res.status(200).json({
       success: true,
@@ -51,11 +50,10 @@ router.post('/signup', (req, res) => {
 
     mailer.send("Compte POP créé avec succès",
       req.body.email,
-      `
-      Votre compte a été créé avec succès. 
+      ` Votre compte a été créé avec succès. 
       Votre mot de passe temporaire est ${password}. 
-      Changez le dès que possible ici http://blabla
-      Thanks !
+      Changez le dès que possible
+      Merci !
       `)
   })
 })
@@ -67,16 +65,69 @@ router.get('/', (req, res) => {
   });
 })
 
+router.post('/updatePassword', (req, res) => {
+  const { email, ppwd, pwd1, pwd2 } = req.body;
+
+  if (!pwd1) {
+    return res.status(401).send({
+      success: false,
+      msg: `Le nouveau mot de passe ,ne peut être vide`
+    })
+  }
+
+  console.log(ppwd, pwd1, pwd2)
+  if (pwd1 !== pwd2) {
+    return res.status(401).send({
+      success: false,
+      msg: `Les mots de passe ne sont pas identiques`
+    })
+  }
+
+
+  User.findOne({ email }, function (err, user) {
+    if (err) throw err
+
+    if (!user) {
+      res.status(401).send({
+        success: false,
+        msg: `La mise à jour du mot de passe à échouée. Utilisateur ${email} introuvable`
+      })
+    } else {
+      user.comparePassword(ppwd, function (err, isMatch) {
+        if (isMatch && !err) {
+
+          user.set({ password: pwd1 });
+
+          user.save(function (err) {
+            if (err) throw err
+            return res.status(200).send({
+              success: true,
+              msg: `La mise à jour du mot de passe à été effectuée avec succès`
+            })
+          });
+
+        } else {
+          res.status(401).send({
+            success: false,
+            msg: `La mise à jour du mot de passe à échouée. Utilisateur introuvable`
+          })
+        }
+      })
+    }
+  })
+})
+
+
 router.post('/signin', (req, res) => {
   User.findOne({
-    username: req.body.username
+    email: req.body.email
   }, function (err, user) {
     if (err) throw err
 
     if (!user) {
       res.status(401).send({
         success: false,
-        msg: 'Authentication failed. User not found.'
+        msg: `L'authentification à échouée. Utilisateur ${req.body.email} introuvable`
       })
     } else {
       user.comparePassword(req.body.password, function (err, isMatch) {
