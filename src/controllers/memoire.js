1eb39660ba1e23f390f90a66c3e2849cd4cff671
goffle <se.legoff@gmail.com>
2018-08-28 13:21:11 +0200

    start on memoire

--- src/controllers/memoire.js
+++ src/controllers/memoire.js
@@ -2,8 +2,60 @@ const express = require('express')
 const router = express.Router()
 const multer = require('multer')
 const upload = multer({ dest: 'uploads/' })
-const Memoire = require('../models/memoire')
+const Memoire = require('../models/memoire');
+const Merimee = require('../models/merimee');
+const Palissy = require('../models/palissy');
 const { uploadFile, formattedNow } = require('./utils')
+
+function findCollection(ref) {
+  const prefix = ref.substring(0, 2);
+  switch (prefix) {
+    case 'EA':
+    case 'PA':
+    case 'IA':
+      return Merimee;
+    case 'IM':
+    case 'PM':
+      return Palissy;
+    default:
+      return "";
+  }
+}
+
+function updateMerimeeOrPalissyNotice(memoire) {
+  return new Promise(async (resolve, reject) => {
+    console.log(memoire)
+    const collection = findCollection(memoire.LBASE);
+
+    if (!memoire.LBASE) {
+      console.log(`No link LBASE ${memoire.REF}`)
+      resolve({ success: false, msg: `No link LBASE ${memoire.LBASE}` });
+      return;
+    }
+
+    if (!collection) {
+      console.log(`No collection ${memoire.LBASE}`)
+      resolve({ success: false, msg: `No collection ${memoire.LBASE}` });
+      return;
+    }
+    const notice = await collection.findOne({ REF: memoire.LBASE });
+
+    if (!notice) {
+      console.log(`No notice ${memoire.LBASE}`)
+      resolve({ success: false, msg: `No notice ${memoire.LBASE}` });
+      return;
+    }
+
+    let isInArray = notice.MEMOIRE.some(function (doc) {
+      return doc.equals(about.id);
+    });
+
+    console.log("notice.MEMOIRE", notice.MEMOIRE)
+    resolve();
+
+  })
+}
+
 
 router.put('/:ref', upload.any(), (req, res) => {
   const ref = req.params.ref
@@ -15,7 +67,12 @@ router.put('/:ref', upload.any(), (req, res) => {
   for (let i = 0; i < req.files.length; i++) {
     arr.push(uploadFile(`memoire/${notice.REF}/${req.files[i].originalname}`, req.files[i]))
   }
-  arr.push(Memoire.findOneAndUpdate({ REF: ref }, notice, { upsert: true, new: true }))
+  arr.push(Memoire.findOneAndUpdate({ REF: ref }, notice, { upsert: true, new: true }));
+
+
+  arr.push(updateMerimeeOrPalissyNotice(notice));
+
+
 
   Promise.all(arr).then(() => {
     res.sendStatus(200)
@@ -35,11 +92,16 @@ router.post('/', upload.any(), (req, res) => {
 
     arr.push(uploadFile(`memoire/${notice.REF}/${req.files[i].originalname}`, req.files[i]))
   }
+
+
+  arr.push(updateMerimeeOrPalissyNotice(notice));
+
   const obj = new Memoire(notice);
   arr.push(obj.save())
   Promise.all(arr).then(() => {
     res.send({ success: true, msg: "OK" })
   }).catch((e) => {
+    console.log(e)
     res.sendStatus(500)
   })
 })
