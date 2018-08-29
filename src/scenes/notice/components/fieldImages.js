c73809324babe99a9143115a004dfcca3aab399d
goffle <se.legoff@gmail.com>
2018-08-29 13:23:28 +0200

    update fieldsImages

--- src/scenes/notice/components/fieldImages.js
+++ src/scenes/notice/components/fieldImages.js
@@ -3,6 +3,7 @@ import { Field } from 'redux-form'
 import Dropzone from 'react-dropzone';
 import { Row, Col, Modal } from 'reactstrap';
 import Viewer from 'react-viewer';
+import { Link } from 'react-router-dom';
 import 'react-viewer/dist/index.css';
 
 import { bucket_url } from '../../../config';
@@ -26,19 +27,26 @@ class FieldImages extends React.Component {
         }
 
         const arr = this.props.input.value.map((e, i) => {
+            //If its a MEMOIRE STYLE 
+            if (e instanceof Object) {
+                const source = e.url;
+                const key = e.ref;
+                console.log('SOURCE', source)
+                return (
+                    <Col className="image" key={key}>
+                        {source ? <img onClick={() => this.setState({ selected: i })} src={source} alt={e} className="img-fluid w-100" /> : <div className="no-image">No Image</div>}
+                        <Link to={`/notice/memoire/${key}`}>{key}</Link>
+                    </Col>
+                )
+            }
+
+            //IF ITS JUST A LINK
             let source = this.props.external ? `${e}` : `${bucket_url}${e}`;
-
-
             let key = e;
-            
             if (e instanceof File) {
                 source = e.preview;
                 key = e.name;
-            }else if(e instanceof Object){
-                source = e.url;
-                key = e.ref;
             }
-
             return (
                 <Col key={key}>
                     <img onClick={() => this.setState({ selected: i })} src={source} alt={e} className="img-fluid w-100" />
@@ -65,6 +73,8 @@ class FieldImages extends React.Component {
         const images = this.props.input.value.map((e, i) => {
             if (e instanceof File) {
                 return { src: e.preview, alt: e.name }
+            } else if (e instanceof Object) {
+                return { src: e.url, alt: e.ref }
             } else {
                 return { src: this.props.external ? `${e}` : `${bucket_url}${e}`, alt: e }
             }
