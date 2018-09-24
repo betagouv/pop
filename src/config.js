22cf5265e82900c8d02fd2d57339ab0dc9f31071
goffle <se.legoff@gmail.com>
2018-09-24 12:07:13 +0200

    update config for ES proxy

--- src/config.js
+++ src/config.js
@@ -2,22 +2,20 @@
 
 let env = null || process.env.NODE_ENV;
 
-console.log('ENV DETECTED : ', env)
-
 let api_url = 'http://127.0.0.1:3000';
-let es_url = 'http://127.0.0.1:9200';
+let es_url = 'http://127.0.0.1:3000/search/';
 let bucket_url = 'https://s3.eu-west-3.amazonaws.com/pop-phototeque-dev/';
 
 switch (env) {
   case 'production':
     api_url = 'http://pop-api.eu-west-3.elasticbeanstalk.com';
-    es_url = 'https://search-pop-j3zoezftjmyiagfgm76ckgu2xy.eu-west-3.es.amazonaws.com';
+    es_url = 'http://pop-api.eu-west-3.elasticbeanstalk.com/search/';
     bucket_url = 'https://s3.eu-west-3.amazonaws.com/pop-phototeque/';
     break;
 
   case 'staging':
     api_url = 'http://pop-api-staging.eu-west-3.elasticbeanstalk.com';
-    es_url = 'https://search-pop-staging-zukwe7tuull7zntiuqs3mp3gr4.eu-west-3.es.amazonaws.com';
+    es_url = 'http://pop-api-staging.eu-west-3.elasticbeanstalk.com/search/';
     bucket_url = 'https://s3.eu-west-3.amazonaws.com/pop-phototeque-staging/';
     break;
 }
