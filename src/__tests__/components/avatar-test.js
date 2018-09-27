133db76824550abde9b6b10d34fa18fafe231c16
Raphaël Huchet <rap2hpoutre@users.noreply.github.com>
2018-09-27 14:15:28 +0200

    jest (#1)
    * add jest
    
    * add some tests

---
+++ src/__tests__/components/avatar-test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Avatar from '../../components/avatar';
+import renderer from 'react-test-renderer';
+
+test('Avatar displays two letters (FB)', () => {
+  const component = renderer.create(<Avatar email = "foo.bar@example.org" />);
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
---
+++ src/__tests__/entities/palissy-test.js
@@ -0,0 +1,23 @@
+import Palissy from '../../entities/palissy';
+import utils from '../../scenes/import/utils';
+import fs from 'fs'
+
+test('Create new Palissy entity without errors from file palissy-mh-valid-UTF-8.csv', async () => {
+
+  const contents = new Blob([fs.readFileSync(__dirname + '/../__notices__/palissy-mh-valid-UTF-8.csv', 'utf-8')]);
+  console.log(await utils.readFile(contents, 'UTF-8', res => { console.log(res) }));
+  const csv = await utils.readCSV(contents, '|', 'UTF-8');
+  const notices = csv.map(notice => {
+    expect(notice.REF.indexOf('PM')).not.toBe(-1);
+    return new Palissy(notice)
+  });
+  expect(notices).toHaveLength(3);
+  notices.forEach(notice => {
+    // This notice is valid and must have no errors.
+    expect(notice._errors).toHaveLength(0);
+    // VIDEO must be parsed to array
+    expect(notice.VIDEO.value).toBeInstanceOf(Array);
+    expect(notice.VIDEO.value.length).toBeGreaterThanOrEqual(1);
+  });
+  expect(notices[2].AUTP.value).toBe('Lefébure, Thierry');
+});
