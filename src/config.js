b9d8f07b0fb9782b2bd97969db501cced87da642
goffle <se.legoff@gmail.com>
2018-09-28 16:16:36 +0200

    update config

--- src/config.js
+++ src/config.js
@@ -1,16 +1,24 @@
-let forceProd = true;
+
+let env = null || process.env.NODE_ENV;
 
 let api_url = 'http://127.0.0.1:3000';
-if (forceProd || process.env.NODE_ENV === 'production') {
-  api_url = 'http://pop-api.eu-west-3.elasticbeanstalk.com';
+let es_url = 'http://127.0.0.1:3000/search/';
+let bucket_url = 'https://s3.eu-west-3.amazonaws.com/pop-phototeque-dev/';
+
+switch (env) {
+  case 'production':
+    api_url = 'http://pop-api.eu-west-3.elasticbeanstalk.com';
+    es_url = 'http://pop-api.eu-west-3.elasticbeanstalk.com/search/';
+    bucket_url = 'https://s3.eu-west-3.amazonaws.com/pop-phototeque/';
+    break;
+
+  case 'staging':
+    api_url = 'http://pop-api-staging.eu-west-3.elasticbeanstalk.com';
+    es_url = 'http://pop-api-staging.eu-west-3.elasticbeanstalk.com/search/';
+    bucket_url = 'https://s3.eu-west-3.amazonaws.com/pop-phototeque-staging/';
+    break;
 }
 
-let bucket_url = 'https://s3.eu-west-3.amazonaws.com/pop-phototeque/';
-
-let es_url = 'http://127.0.0.1:3000/search';
-if (forceProd || process.env.NODE_ENV === 'production') {
-  es_url = 'http://pop-api.eu-west-3.elasticbeanstalk.com/search';
-}
 
 module.exports = {
   api_url,
