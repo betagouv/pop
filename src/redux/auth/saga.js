f5602e7f9424a208a560808d4a1f5ff1baa83577
goffle <se.legoff@gmail.com>
2018-10-01 11:20:40 +0200

    Auto stash before merge of "master" and "origin/master"

--- src/redux/auth/saga.js
+++ src/redux/auth/saga.js
@@ -1,11 +1,10 @@
-import { all, takeEvery, put } from 'redux-saga/effects';
-import { push } from 'react-router-redux';
-import actions from './actions';
+import { all, takeEvery, put } from "redux-saga/effects";
+import { push } from "react-router-redux";
+import actions from "./actions";
 
-import api from '../../services/api';
+import api from "../../services/api";
 
 export function* signin({ email, password }) {
-
   try {
     const { user, token, success, msg } = yield api.signin(email, password);
     if (!success) {
@@ -14,41 +13,55 @@ export function* signin({ email, password }) {
     }
 
     yield put({ type: actions.SIGNIN_SUCCESS, user, token });
-    localStorage.setItem('token', token)
+    localStorage.setItem("token", token);
+
+    if (
+      process.env.NODE_ENV === "production" ||
+      process.env.NODE_ENV === "staging"
+    ) {
+      Raven.setUserContext({ email });
+    }
 
     amplitude.getInstance().setUserId(email);
 
-    yield put(push('/'));
+    yield put(push("/"));
   } catch (e) {
     yield put({ type: actions.SIGNIN_ERROR, error: e });
   }
 }
 
 export function* signinByToken() {
-  const token = localStorage.getItem('token')
+  const token = localStorage.getItem("token");
   if (token) {
-    let response = null
+    let response = null;
     try {
       response = yield api.getUser(token);
     } catch (e) {}
     if (response && response.user) {
+      if (
+        process.env.NODE_ENV === "production" ||
+        process.env.NODE_ENV === "staging"
+      ) {
+        Raven.setUserContext({ emai: response.user.email });
+      }
+
       yield put({ type: actions.SIGNIN_SUCCESS, user: response.user, token });
       return;
     }
-    localStorage.removeItem('token')
+    localStorage.removeItem("token");
   }
   yield put({ type: actions.SIGNIN_FAILED });
 }
 
 export function* logOut() {
-  localStorage.removeItem('token')
-  yield put(push('/'));
+  localStorage.removeItem("token");
+  yield put(push("/"));
 }
 
 export default function* rootSaga() {
   yield all([
     takeEvery(actions.SIGNIN_REQUEST, signin),
     takeEvery(actions.LOGOUT, logOut),
-    takeEvery(actions.SIGNIN_BY_TOKEN_REQUEST, signinByToken),
+    takeEvery(actions.SIGNIN_BY_TOKEN_REQUEST, signinByToken)
   ]);
 }
