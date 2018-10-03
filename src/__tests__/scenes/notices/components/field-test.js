13d917dacc8c2010c05b3416807a8ab8f737bbbe
rap2h <raphaelht@gmail.com>
2018-10-03 13:47:57 +0200

    tests

--- src/__tests__/scenes/notices/components/field-test.js
+++ src/__tests__/scenes/notices/components/field-test.js
@@ -8,6 +8,6 @@ describe('Field suite', () => {
     const hello = shallow(<Field title="hello" content="mycontent" />);
     expect(hello.find('#hello')).toHaveLength(1);
     expect(hello.find('#hello').text()).toContain('hello');
-    expect(hello.find('#hello>span').text()).toContain('mycontent');
+    expect(hello.find('#hello>p').text()).toContain('mycontent');
   });
 });
