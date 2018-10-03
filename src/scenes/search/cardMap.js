748675e0eb631e175139aa8c019d1bde4825601e
Gethi <bigard.antoine@gmail.com>
2018-10-03 09:25:19 +0200

    Shave title cardMap

--- src/scenes/search/cardMap.js
+++ src/scenes/search/cardMap.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { image } from "./image";
+import shave from 'shave';
 import "./cardMap.css";
 
 const joinData = f => {
@@ -22,80 +23,92 @@ const bases = {
 
 const capitalizeFirstLetter = s => s.charAt(0).toUpperCase() + s.slice(1);
 
-export default ({ data }) => {
-  let content = {};
-  switch (data._index) {
-    case "joconde":
-    case "mnr":
-      content = {
-        title: data.TICO || data.TITR,
-        ref: data.REF,
-        categories: data.DENO ? data.DENO.join(", ") : "",
-        author: joinData([data.AUTR, data.ECOL, data.EPOQ]),
-        data: data.PERI,
-        loc: data.LOCA
-      };
-      break;
-    case "merimee":
-    case "palissy":
-      content = {
-        title: data.TICO || data.TITR,
-        ref: data.REF,
-        categories: data.DENO ? data.DENO.join(", ") : "",
-        author: data.AUTR,
-        data: data.SCLE,
-        loc: data.LOCA
-          ? joinData([data.LOCA])
-          : joinData([data.REG, data.DPT, data.COM]),
-        spe: data.DPRO
-      };
-      break;
-    case "memoire":
-      content = {
-        title: data.TICO,
-        subtitle: joinData([
-          data.OBJET,
-          data.EDIF,
-          data.LEG,
-          data.DATOEU,
-          data.DATOEU ? "" : data.SCLE
-        ]),
-        ref: data.REF,
-        categories: data.TECH,
-        author: data.AUTP,
-        data: joinData([data.DATPV, data.DATOR]),
-        loc: data.LOCA
-      };
-      break;
-  }
-  return (
-    <Link
-      style={{ textDecoration: "none" }}
-      to={`/notice/${data._index}/${data.REF}`}
-      className="map-card"
-      key={data.REF}
-    >
-      <div className="thumbnail">{image(data)}</div>
-      <div className="content">
-        <div style={{ display: "flex" }}>
-          <h2>
-            {capitalizeFirstLetter(content.title)}
-            <br />
-            <small>{content.categories}</small>
-          </h2>
-          <span>
+export default class CardMap extends React.Component {
+    constructor(props) {
+        super(props);
+        this.titleRef = React.createRef();
+    }
+
+    componentDidMount() {
+        shave(this.titleRef.current, 70);
+    }
+
+    render() {
+      const { data } =  this.props;
+        let content = {};
+        switch (data._index) {
+            case "joconde":
+            case "mnr":
+                content = {
+                    title: data.TICO || data.TITR,
+                    ref: data.REF,
+                    categories: data.DENO ? data.DENO.join(", ") : "",
+                    author: joinData([data.AUTR, data.ECOL, data.EPOQ]),
+                    data: data.PERI,
+                    loc: data.LOCA
+                };
+                break;
+            case "merimee":
+            case "palissy":
+                content = {
+                    title: data.TICO || data.TITR,
+                    ref: data.REF,
+                    categories: data.DENO ? data.DENO.join(", ") : "",
+                    author: data.AUTR,
+                    data: data.SCLE,
+                    loc: data.LOCA
+                        ? joinData([data.LOCA])
+                        : joinData([data.REG, data.DPT, data.COM]),
+                    spe: data.DPRO
+                };
+                break;
+            case "memoire":
+                content = {
+                    title: data.TICO,
+                    subtitle: joinData([
+                        data.OBJET,
+                        data.EDIF,
+                        data.LEG,
+                        data.DATOEU,
+                        data.DATOEU ? "" : data.SCLE
+                    ]),
+                    ref: data.REF,
+                    categories: data.TECH,
+                    author: data.AUTP,
+                    data: joinData([data.DATPV, data.DATOR]),
+                    loc: data.LOCA
+                };
+                break;
+        }
+        return (
+            <Link
+                style={{ textDecoration: "none" }}
+                to={`/notice/${data._index}/${data.REF}`}
+                className="map-card"
+                key={data.REF}
+            >
+                <div className="thumbnail">{image(data)}</div>
+                <div className="content">
+                    <div style={{ display: "flex" }}>
+                        <h2 ref={this.titleRef}>
+                            {capitalizeFirstLetter(content.title)}
+                            <br />
+                            <small>{content.categories}</small>
+                        </h2>
+                        <span>
             <span className="base">{bases[data._index]}</span>
             <br />
-            {data.REF}
+                            {data.REF}
           </span>
-        </div>
-        <p>{content.subtitle}</p>
-        <div>
-          <p>{content.author}</p>
-          <p>{content.data}</p>
-          <p>{content.loc}</p>
-        </div>
-      </div>
-    </Link>
-  );
+                    </div>
+                    <p>{content.subtitle}</p>
+                    <div>
+                        <p>{content.author}</p>
+                        <p>{content.data}</p>
+                        <p>{content.loc}</p>
+                    </div>
+                </div>
+            </Link>
+        );
+    }
 };
