e4ab43cd46106bff7db7184a49d7881dd3031ee3
goffle <se.legoff@gmail.com>
2018-10-12 18:01:37 +0200

    sign mongoosastic requests

--- src/elasticsearch.js
+++ src/elasticsearch.js
@@ -2,15 +2,15 @@ const AWS = require("aws-sdk");
 const { esUrl } = require("./config.js");
 
 const getElasticInstance = () => {
-  if (elasticClient) {
-    return elasticClient;
-  }
-
   let options = {
     hosts: [esUrl],
     connectionClass: require("http-aws-es"),
     awsConfig: new AWS.Config({
-      credentials: new AWS.Credentials("XXXXXXX", "YYYYYYY")
+      region: "eu-west-3",
+      credentials: new AWS.Credentials(
+        process.env.AWS_ACCESS_KEY_ID,
+        process.env.AWS_SECRET_ACCESS_KEY
+      )
     })
   };
   let elasticClient = require("elasticsearch").Client(options);
