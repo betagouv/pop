32e7d6450a450cbb96c010d0ba0b26b0fabca76d
goffle <se.legoff@gmail.com>
2018-10-18 12:50:15 +0200

    use env for sentry credentials

--- src/sentry.js
+++ src/sentry.js
@@ -1,9 +1,8 @@
 const Sentry = require("@sentry/node");
 
 if (process.env.NODE_ENV !== "development") {
-  console.log('PLOP')
   Sentry.init({
-    dsn: "https://9cca185065d74dbd9e05987036f2d16d:52144c3d0eb447c4a370ad6031b4bcbb@sentry.data.gouv.fr/21",
+    dsn: process.env.SENTRY_DSN,
     release: "pop-api@" + require("../package.json").version,
     environment: process.env.NODE_ENV
   });
