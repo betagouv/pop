e3ba73d6c5d3d17dacd4da9f769ef0593ceb50b2
goffle <se.legoff@gmail.com>
2018-10-18 12:46:19 +0200

    change key

--- src/sentry.js
+++ src/sentry.js
@@ -3,7 +3,7 @@ const Sentry = require("@sentry/node");
 if (process.env.NODE_ENV !== "development") {
   console.log('PLOP')
   Sentry.init({
-    dsn: "https://9cca185065d74dbd9e05987036f2d16d@sentry.data.gouv.fr/21",
+    dsn: "https://9cca185065d74dbd9e05987036f2d16d:52144c3d0eb447c4a370ad6031b4bcbb@sentry.data.gouv.fr/21",
     release: "pop-api@" + require("../package.json").version,
     environment: process.env.NODE_ENV
   });
