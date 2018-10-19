f19bb36a1ccbbee914860ff60977cede82d61b51
rap2h <raphaelht@gmail.com>
2018-10-19 12:00:19 +0200

    hop

--- src/elasticsearch/index.js
+++ src/elasticsearch/index.js
@@ -1,17 +1,19 @@
 const program = require("commander");
 const inquirer = require("inquirer");
-const chalk = require("chalk");
 const es = require("../elasticsearch")();
-
-const warn = text => console.log(chalk.orange(text));
-const success = text => console.log(chalk.green(text));
-const info = text => console.log(chalk.blue(text));
-const fatal = text => console.log(chalk.red(text));
+const Listr = require("listr");
+const fs = require("fs");
 
 async function run() {
   program
     .version("0.1.0")
     .option("-f, --force", "Force reindex")
+    .option(
+      "-i --indices <indices>",
+      "The name of the indices",
+      val => val.split(","),
+      "joconde,memoire,merimee,mnr,palissy".split(",")
+    )
     .parse(process.argv);
 
   if (!program.force) {
@@ -25,72 +27,114 @@ async function run() {
     }
   }
 
-  const available = await es.ping({ requestTimeout: "10s" });
-  if (!available) {
-    warn("Elasticsearch not available.");
-    process.exit(1);
-  }
-
-  ["joconde"].map(async db => {
-    info(`Processing ${db}...`);
-    
-    // The original index name
-    const rootDbname = db;
-    // A new index name
-    const dbname = db + new Date().valueOf();
-
-    // Create a new index with correct settings and mappings.
-    let response = await es.indices.create({
-      index: dbname,
-      body: require(`./indices/${db}.js`)
-    });
-    if (!response.acknowledged) {
-      warn("Failed to create new index.");
-      process.exit(1);
-    }
-    // Reindex only if previous index exists,
-    // otherwise, the index is empty, so nothing to do.
-    if (await es.indices.exists({ index: rootDbname })) {
-      response = await es.reindex({
-        requestTimeout: "1h",
-        body: {
-          source: { index: rootDbname },
-          dest: { index: dbname }
+  const tasks = new Listr([
+    {
+      title: "Ping",
+      task: async () => {
+        const available = await es.ping({ requestTimeout: 10000 });
+        if (!available) {
+          throw new Error("Elasticsearch not available.");
         }
-      });
-      if (response.failures.length) {
-        warn("Oups. There where some failures.");
-        warn(response);
-        process.exit(1);
       }
-      // Find previous db name.
-      const aliases = await es.indices.getAlias({ index: "*" });
-      const previousDbname =
-        Object.keys(aliases).find(key => aliases[key].aliases[rootDbname]) ||
-        rootDbname;
-      // Create alias to the rootname and remove previous db.
-      response = await es.indices.updateAliases({
-        body: {
-          actions: [
-            { add: { index: dbname, alias: rootDbname } },
-            { remove_index: { index: previousDbname } }
-          ]
-        }
-      });
-    } else {
-      // Create alias to the rootname.
-      response = await es.indices.updateAliases({
-        body: {
-          actions: [{ add: { index: dbname, alias: rootDbname } }]
-        }
-      });
     }
-    if (!response.acknowledged) {
-      fatal("Fatal error, RED FLAG! Alias rename failed. Rebuild it NOW!");
-      process.exit(1);
-    }
-    success("Fine!");
+  ]);
+  program.indices.map(async db => {
+    tasks.add(
+      {
+        title: `Processing ${db}...`,
+        task: async () => {
+          // The original index name
+          const rootDbname = db;
+          // A new index name
+          const dbname = db + new Date().valueOf();
+
+          // Task list
+          return new Listr([
+            {
+              title: "Create a new index with correct settings and mappings.",
+              task: async ctx => {
+                const filename = `./indices/${db}.js`;
+                if (!fs.existsSync(`${__dirname}/${filename}`)) {
+                  throw new Error(`Failed to locate ${__dirname}/${filename}`);
+                }
+                const response = await es.indices.create({
+                  index: dbname,
+                  body: require(filename)
+                });
+                if (!response.acknowledged) {
+                  throw new Error("Failed to create new index.");
+                }
+                ctx.rootExists = await es.indices.exists({ index: rootDbname });
+              }
+            },
+            {
+              title: "Reindex",
+              enabled: ctx => ctx.rootExists === true,
+              task: async () => {
+                const response = await es.reindex({
+                  timeout: "1h",
+                  body: {
+                    source: { index: rootDbname },
+                    dest: { index: dbname }
+                  }
+                });
+                if (response.failures.length) {
+                  console.error(response);
+                  throw new Error("Oups. There where some failures.");
+                }
+              }
+            },
+            {
+              title: "Create alias to the rootname and remove previous db",
+              enabled: ctx => ctx.rootExists === true,
+              task: async () => {
+                // Find previous db name.
+                const aliases = await es.indices.getAlias({ index: "*" });
+                const previousDbname =
+                  Object.keys(aliases).find(
+                    key => aliases[key].aliases[rootDbname]
+                  ) || rootDbname;
+                // Create alias to the rootname and remove previous db.
+                const response = await es.indices.updateAliases({
+                  body: {
+                    actions: [
+                      { add: { index: dbname, alias: rootDbname } },
+                      { remove_index: { index: previousDbname } }
+                    ]
+                  }
+                });
+                if (!response.acknowledged) {
+                  console.error(response);
+                  throw new Error("Fatal error!");
+                }
+              }
+            },
+            {
+              title: "Create alias to the rootname",
+              enabled: ctx => ctx.rootExists === false,
+              task: async () => {
+                const response = (response = await es.indices.updateAliases({
+                  body: {
+                    actions: [{ add: { index: dbname, alias: rootDbname } }]
+                  }
+                }));
+                if (!response.acknowledged) {
+                  console.error(response);
+                  throw new Error("Fatal error!");
+                }
+              }
+            }
+          ]);
+        }
+      },
+      { concurrent: true, exitOnError: false }
+    );
   });
+  try {
+    await tasks.run();
+  } catch (e) {
+    console.error(e);
+  }
 }
 
 run();
