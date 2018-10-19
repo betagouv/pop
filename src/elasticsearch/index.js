5045df63d0cf2f9c3e2d95ff93e647de31045a02
rap2h <raphaelht@gmail.com>
2018-10-19 10:26:28 +0200

    listr

--- src/elasticsearch/index.js
+++ src/elasticsearch/index.js
@@ -3,6 +3,11 @@ const inquirer = require("inquirer");
 const chalk = require("chalk");
 const es = require("../elasticsearch")();
 
+const warn = text => console.log(chalk.orange(text));
+const success = text => console.log(chalk.green(text));
+const info = text => console.log(chalk.blue(text));
+const fatal = text => console.log(chalk.red(text));
+
 async function run() {
   program
     .version("0.1.0")
@@ -20,15 +25,15 @@ async function run() {
     }
   }
 
-  const available = await es.ping({
-    requestTimeout: 30000
-  });
+  const available = await es.ping({ requestTimeout: "10s" });
   if (!available) {
-    console.log(chalk.orange("Elasticsearch not available."));
+    warn("Elasticsearch not available.");
     process.exit(1);
   }
 
   ["joconde"].map(async db => {
+    info(`Processing ${db}...`);
+    
     // The original index name
     const rootDbname = db;
     // A new index name
@@ -40,21 +45,22 @@ async function run() {
       body: require(`./indices/${db}.js`)
     });
     if (!response.acknowledged) {
-      console.log(chalk.orange("Failed to create new index."));
+      warn("Failed to create new index.");
       process.exit(1);
     }
     // Reindex only if previous index exists,
     // otherwise, the index is empty, so nothing to do.
     if (await es.indices.exists({ index: rootDbname })) {
       response = await es.reindex({
+        requestTimeout: "1h",
         body: {
           source: { index: rootDbname },
           dest: { index: dbname }
         }
       });
       if (response.failures.length) {
-        console.log(chalk.orange("Oups. There where some failures."));
-        console.log(response);
+        warn("Oups. There where some failures.");
+        warn(response);
         process.exit(1);
       }
       // Find previous db name.
@@ -80,14 +86,10 @@ async function run() {
       });
     }
     if (!response.acknowledged) {
-      console.log(
-        chalk.red(
-          "Fatal error, RED FLAG, run away! Alias rename failed. Rebuild it NOW!"
-        )
-      );
+      fatal("Fatal error, RED FLAG! Alias rename failed. Rebuild it NOW!");
       process.exit(1);
     }
-    console.log(chalk.green("Fine!"));
+    success("Fine!");
   });
 }
 
