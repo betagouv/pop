d72d836dd04772c0502c29d39ee7080979770f0d
goffle <se.legoff@gmail.com>
2018-10-24 18:58:47 +0200

    Auto stash before revert of "fix overflow of viewer"

--- src/scenes/search/subComponents/Mosaique/Mosaique.js
+++ src/scenes/search/subComponents/Mosaique/Mosaique.js
@@ -4,26 +4,24 @@ import { ReactiveList } from "@appbaseio/reactivesearch";
 import CardMosaique from "./CardMosaique";
 
 export default ({ filter }) => (
-    <ReactiveList
-        componentId="results"
-        react={{
-        and: filter
-        }}
-        onResultStats={(total, took) => {
-            if (total === 1) {
-                return `1 résultat`;
-            }
-            return `${total} résultats`;
-        }}
-        onNoResults="Aucun résultat trouvé."
-        loader="Préparation de l'affichage des résultats..."
-        dataField=""
-        URLParams={true}
-        size={18}
-        className="mosaique-view"
-        onData={data => (
-        <CardMosaique key={data.REF} data={data} />
-        )}
-        pagination={true}
-    />
+  <ReactiveList
+    componentId="results"
+    react={{
+      and: filter
+    }}
+    onResultStats={(total, took) => {
+      if (total === 1) {
+        return `1 résultat`;
+      }
+      return `${total} résultats`;
+    }}
+    onNoResults="Aucun résultat trouvé."
+    loader="Préparation de l'affichage des résultats..."
+    dataField=""
+    URLParams={true}
+    size={18}
+    className="mosaique-view"
+    onData={data => <CardMosaique key={data.REF} data={data} />}
+    // pagination={true}
+  />
 );
