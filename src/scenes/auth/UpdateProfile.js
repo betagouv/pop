50b8188572b8487dc0a423c89ef7af8a1b32e2b1
Gethi <bigard.antoine@gmail.com>
2018-11-06 16:33:01 +0100

    Change form

---
+++ src/scenes/auth/UpdateProfile.js
@@ -0,0 +1,274 @@
+import React, { Component } from "react";
+import { Container, Button, Input } from "reactstrap";
+import { connect } from "react-redux";
+import { Redirect } from "react-router-dom";
+import { history } from "../../redux/store";
+
+import api from "../../services/api";
+import Loader from "../../components/loader";
+
+import authAction from "./../../redux/auth/actions";
+const { logout } = authAction;
+
+    /*email: "antoine.bigard@beta.gouv.fr"
+    group: "admin"
+    hasResetPassword: true
+    institution: "POP"
+    lastConnectedAt: "2018-11-06T12:32:01.710Z"
+    nom: "Bigard"
+    prenom: "Antoine"
+    role: "administrateur"
+    _id: "5be17cdb2ef6b005c90be1e3"*/
+
+class UpdateProfile extends Component {
+  state = {
+    ppwd: "",
+    ppwd1: "",
+    ppwd2: "",
+    loading: false,
+    done: false,
+    error: "",
+    hasResetPassword: false,
+    nom: "",
+    prenom: "",
+    institution: "",
+    group: "",
+    role: "",
+  };
+
+  componentWillMount() {
+    const { user } = this.props;
+    console.log(user)
+    const { hasResetPassword, nom, prenom, institution, group, role } = user;
+    this.setState({ hasResetPassword, nom, prenom, institution, group, role });
+  }
+
+  componentDidUpdate(prevProps) {
+    const { user } = this.props;
+    const { user: prevUser } = prevProps;
+    if(user !== prevUser) {
+      const { hasResetPassword, nom, prenom, institution, group, role } = user;
+      this.setState({ hasResetPassword, nom, prenom, institution, group, role });
+    }
+  }
+
+  resetPasswordMessage() {
+    if (this.props.hasResetPassword) {
+      return <div />;
+    }
+    return (
+      <p>
+        Vous n'avez pas encore changé votre mot de passe. Pour votre sécurité,
+        vous devez le changer avant de continuer.
+      </p>
+    );
+  }
+
+
+  updateProfile = ()=> {
+    const { ppwd, ppwd1, ppwd2 } = this.state;
+    this.setState({ loading: true });
+    api
+      .updatePassword(this.props.email, ppwd, ppwd1, ppwd2)
+      .then(() => {
+        this.setState({ loading: false, done: true });
+        setTimeout(() => {
+          this.props.logout();
+        }, 5000);
+      })
+      .catch(e => {
+        this.setState({ error: e, loading: false, done: false });
+      });
+  }
+
+  lastConnectedAt() {
+    if (!this.props.lastConnectedAt) {
+      return <div />;
+    }
+    return (
+      <p className="lastConnectedAt">
+        <em>
+          Date de dernière connection:{" "}
+          {new Date(this.props.lastConnectedAt).toLocaleString("fr-FR")}
+        </em>
+      </p>
+    );
+  }
+
+  renderGroups = ()=>{
+    const { group } = this.state;
+    let groupes = [];
+
+    if (group === "admin") {
+      groupes = groupes.concat([
+        "admin",
+        "mnr",
+        "joconde",
+        "mh",
+        "inv",
+        "memoire"
+      ]);
+    } else {
+      groupes.push(group);
+    }
+    return groupes.map((e,i) => <option key={`${e}${i}`}>{e}</option>);
+  }
+
+  renderRoles = ()=> {
+    const { group, role } = this.state;
+    let roles = [];
+    if (role === "administrateur" || group === "admin") {
+      roles = roles.concat(["utilisateur", "producteur", "administrateur"]);
+    }
+
+    return roles.map((e,i) => <option key={`${e}${i}`}>{e}</option>);
+  }
+
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value,
+    });
+  };
+
+  render() {
+    const { email, location } = this.props;
+    const { loading, done, error, nom, prenom, institution, group, role } = this.state;
+    if (loading) {
+      return <Loader />;
+    }
+
+    // if (done) {
+    //   return (
+    //     <Container className="signin">
+    //       <div>
+    //         Votre mot de passe à été changé. <br />
+    //         Vous allez être deconnecté dans 5 secondes ...{" "}
+    //       </div>
+    //     </Container>
+    //   );
+    // }
+
+    if (done) {
+      return (
+        <Container className="signin">
+          <div>
+            Vosinformations ont été changées. <br />
+          </div>
+        </Container>
+      );
+    }
+
+    //If the user is not login, he shouldnt have access to the page. Its a restricted page
+    if (!email) {
+      return (
+        <Redirect
+          to={{
+            pathname: "/auth/signin",
+            state: { from: location }
+          }}
+        />
+      );
+    }
+
+    return (
+      <Container className="signin">
+        <div className="block">
+          <h1>Modifier mes informations</h1>
+          {this.resetPasswordMessage()}
+          <div>{error}</div>
+          <div className="sub-block">
+            <h4>Mon descriptif</h4>
+            <input
+              className="input-field"
+              placeholder="Mon nom"
+              type="text"
+              value={nom}
+              onChange={this.handleChange('nom')}
+            />
+            <input
+              className="input-field"
+              placeholder="Mon prénom"
+              type="text"
+              value={prenom}
+              onChange={this.handleChange('prenom')}
+            />
+            <input
+              className="input-field"
+              placeholder="Institution"
+              type="text"
+              value={institution}
+              onChange={this.handleChange('institution')}
+            />
+          </div>
+          <hr /> 
+          <div className="sub-block">
+          <h4>Mes groupes et  roles</h4>
+            <Input
+              type="select"
+              value={group}
+              onChange={this.handleChange('group')}
+              className="input-field select-field"
+            >
+              {this.renderGroups()}
+            </Input>
+            <Input
+              type="select"
+              value={role}
+              onChange={this.handleChange('role')}
+              className="input-field select-field"
+            >
+              {this.renderRoles()}
+            </Input>
+          </div>
+          <hr /> 
+          <div className="sub-block">
+            <h4>Mon mot de passe</h4>
+            <input
+              className="input-field"
+              placeholder="Mot de passe actuel"
+              type="password"
+              value={this.state.ppwd}
+              onChange={e => this.setState({ ppwd: e.target.value })}
+            />
+            <input
+              className="input-field"
+              placeholder="Nouveau Mot de passe"
+              type="password"
+              value={this.state.ppwd1}
+              onChange={e => this.setState({ ppwd1: e.target.value })}
+            />
+            <input
+              className="input-field"
+              placeholder="Nouveau Mot de passe (confirmation)"
+              type="password"
+              value={this.state.ppwd2}
+              onChange={e => this.setState({ ppwd2: e.target.value })}
+            />
+          </div>
+          <hr /> 
+          <Button
+            className="submit-button"
+            onClick={this.updateProfile}
+          >
+            Mettre à jour mes informations
+          </Button>
+          {this.lastConnectedAt()}
+        </div>
+      </Container>
+    );
+  }
+}
+
+const mapStateToProps = ({ Auth }) => {
+  return {
+    user: Auth.user,
+    email: Auth.user ? Auth.user.email : "",
+    hasResetPassword: !!Auth.user && Auth.user.hasResetPassword,
+    lastConnectedAt: !!Auth.user && Auth.user.lastConnectedAt
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { logout }
+)(UpdateProfile);
