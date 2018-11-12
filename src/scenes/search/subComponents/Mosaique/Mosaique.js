1ffd56b08eaad4830ad20999cc0ee8dcee5cd9eb
Raphaël Huchet <rap2hpoutre@users.noreply.github.com>
2018-11-12 10:34:34 +0100

    info about tiles (#15)

--- src/scenes/search/subComponents/Mosaique/Mosaique.js
+++ src/scenes/search/subComponents/Mosaique/Mosaique.js
@@ -10,10 +10,11 @@ export default ({ filter }) => (
       and: filter
     }}
     onResultStats={(total, took) => {
+      const info = "La mosaïque n'affiche par défaut que les notices avec image.";
       if (total === 1) {
-        return `1 résultat`;
+        return <span>1 résultat <i class="text-muted">{info}</i></span>;
       }
-      return `${total} résultats`;
+      return <span>{total} résultats. <i class="text-muted">{info}</i></span>;
     }}
     onNoResults="Aucun résultat trouvé."
     loader="Préparation de l'affichage des résultats..."
