88a8b822e68c863e613f444a6f0e33d661a9bc3e
rap2h <raphaelht@gmail.com>
2018-11-13 14:41:05 +0100

    webpack bail

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,34 @@
-const path = require('path');
+const path = require("path");
 
-const webpack = require('webpack');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ManifestPlugin = require('webpack-manifest-plugin');
-
+const webpack = require("webpack");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ManifestPlugin = require("webpack-manifest-plugin");
 
 module.exports = env => {
-  const mode = env['production'] ? 'production' : 'staging';
-  console.log('MODE :', mode)
+  const mode = env["production"] ? "production" : "staging";
+  console.log("MODE :", mode);
   const plugins = [
     new ManifestPlugin({
-      seed: require('./public/manifest.json')
+      seed: require("./public/manifest.json")
     }),
     new HtmlWebpackPlugin({
-      template: './public/index.html',
-      filename: 'index.html',
-      inject: 'body',
-      favicon: path.join('public/favicon.ico'),
+      template: "./public/index.html",
+      filename: "index.html",
+      inject: "body",
+      favicon: path.join("public/favicon.ico"),
       minify: {
         removeAttributeQuotes: true,
         collapseWhitespace: true,
         html5: true,
         minifyCSS: true,
         removeComments: true,
-        removeEmptyAttributes: true,
+        removeEmptyAttributes: true
       }
     }),
     new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify(mode)
+      "process.env": {
+        NODE_ENV: JSON.stringify(mode)
       }
     }),
     new UglifyJsPlugin({
@@ -40,18 +39,21 @@ module.exports = env => {
     })
   ];
 
-
   return {
-    mode: 'production',
-    entry: ['babel-polyfill', './src/index.js'],
+    mode: "production",
+    entry: ["babel-polyfill", "./src/index.js"],
     devtool: false,
+    bail: true,
     output: {
-      path: path.resolve('build'),
-      filename: '[hash].index.js',
-      publicPath: '/'
+      path: path.resolve("build"),
+      filename: "[hash].index.js",
+      publicPath: "/"
     },
     node: {
-      fs: 'empty'
+      fs: "empty"
+    },
+    performance: {
+      hints: false
     },
     module: {
       rules: [
@@ -61,45 +63,48 @@ module.exports = env => {
         },
         {
           test: /\.js$/,
-          loader: 'babel-loader',
-          include: path.resolve('src'),
+          loader: "babel-loader",
+          include: path.resolve("src"),
           exclude: /node_modules/,
           options: {
-              babelrc: true,
-              cacheDirectory: true,
-              sourceMaps: false,
-              compact: false,
-              presets: [
-                  ['env', {
-                      loose: true,
-                      modules: false,
-                      targets: {
-                          browsers: ['>0.03%']
-                      },
-                      useBuiltIns: true
-                  }],
-              ],
-              plugins: [
-                  'transform-class-properties',
-                  'transform-object-rest-spread'
+            babelrc: true,
+            cacheDirectory: true,
+            sourceMaps: false,
+            compact: false,
+            presets: [
+              [
+                "env",
+                {
+                  loose: true,
+                  modules: false,
+                  targets: {
+                    browsers: [">0.03%"]
+                  },
+                  useBuiltIns: true
+                }
               ]
+            ],
+            plugins: [
+              "transform-class-properties",
+              "transform-object-rest-spread"
+            ]
           }
         },
         {
           test: /\.(gif|png|jpe?g|svg|woff|woff2)$/i,
           exclude: /node_modules/,
           use: [
-            'file-loader',
+            "file-loader",
             {
-              loader: 'image-webpack-loader',
+              loader: "image-webpack-loader",
               options: {
-                bypassOnDebug: true,
-              },
-            },
-          ],
+                bypassOnDebug: true
+              }
+            }
+          ]
         }
       ]
     },
     plugins: plugins
-  }
+  };
 };
