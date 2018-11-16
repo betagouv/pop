d3d17ae0229642a1f5c9cf955e95a32e84d63700
goffle <se.legoff@gmail.com>
2018-11-16 22:11:52 +0100

    copie robotos.txt

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,8 @@ const webpack = require("webpack");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ManifestPlugin = require("webpack-manifest-plugin");
-const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+const SWPrecacheWebpackPlugin = require("sw-precache-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = env => {
   const mode = env["production"] ? "production" : "staging";
@@ -13,6 +14,10 @@ module.exports = env => {
     new ManifestPlugin({
       seed: require("./public/manifest.json")
     }),
+    new CopyWebpackPlugin(
+      [{ from: "./public/robots.txt", to: "./robots.txt" }],
+      {}
+    ),
     new HtmlWebpackPlugin({
       template: "./public/index.html",
       filename: "index.html",
@@ -44,13 +49,13 @@ module.exports = env => {
       // If a URL is already hashed by Webpack, then there is no concern
       // about it being stale, and the cache-busting can be skipped.
       dontCacheBustUrlsMatching: /\.\w{8}\./,
-      filename: 'service-worker.js',
+      filename: "service-worker.js",
       logger(message) {
-        if (message.indexOf('Total precache size is') === 0) {
+        if (message.indexOf("Total precache size is") === 0) {
           // This message occurs for every build and is a bit too noisy.
           return;
         }
-        if (message.indexOf('Skipping static resource') === 0) {
+        if (message.indexOf("Skipping static resource") === 0) {
           // This message obscures real errors so we ignore it.
           // https://github.com/facebookincubator/create-react-app/issues/2612
           return;
@@ -59,13 +64,13 @@ module.exports = env => {
       },
       minify: true,
       // For unknown URLs, fallback to the index page
-      navigateFallback:  '/index.html',
+      navigateFallback: "/index.html",
       // Ignores URLs starting from /__ (useful for Firebase):
       // https://github.com/facebookincubator/create-react-app/issues/2237#issuecomment-302693219
       navigateFallbackWhitelist: [/^(?!\/__).*/],
       // Don't precache sourcemaps (they're large) and build asset manifest:
-      staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/], //, /index\.html$/
-    }),
+      staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/] //, /index\.html$/
+    })
   ];
 
   return {
@@ -122,18 +127,16 @@ module.exports = env => {
         {
           test: /\.(woff|woff2)$/i,
           exclude: /node_modules/,
-          use: [
-            "file-loader",
-          ]
+          use: ["file-loader"]
         },
         {
           test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/, /\.svg$/],
-          loader: require.resolve('url-loader'),
+          loader: require.resolve("url-loader"),
           options: {
             limit: 10000,
-            name: '[name].[hash:8].[ext]',
-          },
-        },
+            name: "[name].[hash:8].[ext]"
+          }
+        }
       ]
     },
     plugins: plugins
