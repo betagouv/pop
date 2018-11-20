df62a4cd17dd90e45ff5eb00a94de661cd2171bd
Raphaël Huchet <rap2hpoutre@users.noreply.github.com>
2018-11-20 10:32:22 +0100

    error boundary (#28)

---
+++ src/components/ErrorBoundary/index.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import "./index.css";
+import roy from '../../assets/roy-lichtenstein.jpg';
+
+export default class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, info) {
+    Raven.captureMessage(error && error.toString(), {
+      extra: info
+    });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <Container className="error-boundary">
+        <Row>
+          <Col>
+          <h1>Oups !</h1>
+          <p>
+            On a entendu comme un petit BOUM dans POP. 
+            <br />
+            L'équipe est dessus,
+            revenez dans quelques instants.
+          </p>
+          </Col>
+          <Col>
+          <img src={roy} alt="I...I'm sorry, Roy Lichtenstein"/><br/>
+          I...I'm sorry, Roy Lichtenstein
+          </Col>
+        </Row>
+          
+        </Container>
+      );
+    }
+
+    return this.props.children;
+  }
+}
--- src/router.js
+++ src/router.js
@@ -1,44 +1,47 @@
-import React from 'react';
-import { Route, Redirect, Switch } from 'react-router-dom';
-import { ConnectedRouter } from 'react-router-redux';
-
-import Header from './scenes/header';
-import Footer from './scenes/footer';
+import React from "react";
+import { Route, Redirect, Switch } from "react-router-dom";
+import { ConnectedRouter } from "react-router-redux";
+import ErrorBoundary from "./components/ErrorBoundary";
+import Header from "./scenes/header";
+import Footer from "./scenes/footer";
 import NotFound from "./components/NotFound";
 
-import Home from './scenes/home';
-import Search from './scenes/search';
-import Opendata from './scenes/opendata';
-import Notice from './scenes/notice';
+import Home from "./scenes/home";
+import Search from "./scenes/search";
+import Opendata from "./scenes/opendata";
+import Notice from "./scenes/notice";
 
-import PiwikReactRouter from 'piwik-react-router';
+import PiwikReactRouter from "piwik-react-router";
 
 import ScrollToTop from "./components/ScrollToTop";
 
 const piwik = PiwikReactRouter({
-	url: 'https://stats.data.gouv.fr',
-	siteId: 63
+  url: "https://stats.data.gouv.fr",
+  siteId: 63
 });
-piwik.push(["setDomains", ["*.pop.beta.gouv","*.pop.culture.gouv.fr","*.production.pop.beta.gouv.fr"]]);
+piwik.push([
+  "setDomains",
+  ["*.pop.beta.gouv", "*.pop.culture.gouv.fr", "*.production.pop.beta.gouv.fr"]
+]);
 
 export default class PublicRoutes extends React.Component {
-
-  componentWillReceiveProps(newProps){
-  }
+  componentWillReceiveProps(newProps) {}
 
   render() {
     return (
       <ConnectedRouter history={piwik.connectToHistory(this.props.history)}>
-        <div className='main'>
+        <div className="main">
           <ScrollToTop />
           <Header />
-          <Switch>
-            <Route exact path={'/'} component={Home} />
-            <Route path={'/search'} component={Search} />
-            <Route path={'/opendata'} component={Opendata} />
-            <Route path={'/notice/:ref'} component={Notice} />
-            <Route component={NotFound} />
-          </Switch>
+          <ErrorBoundary>
+            <Switch>
+              <Route exact path={"/"} component={Home} />
+              <Route path={"/search"} component={Search} />
+              <Route path={"/opendata"} component={Opendata} />
+              <Route path={"/notice/:ref"} component={Notice} />
+              <Route component={NotFound} />
+            </Switch>
+          </ErrorBoundary>
           <Footer />
         </div>
       </ConnectedRouter>
