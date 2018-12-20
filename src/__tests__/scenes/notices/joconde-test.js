42975480eee20b365f7fce072efc2163d04d6e7b
Raphaël Huchet <rap2hpoutre@users.noreply.github.com>
2018-12-20 10:24:05 +0100

    add more tests ("real" joconde document) (#58)
    * add more tests ("real" joconde document)
    
    * fix file mock

--- src/__tests__/scenes/notices/joconde-test.js
+++ src/__tests__/scenes/notices/joconde-test.js
@@ -1,14 +1,52 @@
-import React from 'react';
-import { shallow, mount, render } from 'enzyme';
-import { Row, Col, Container } from 'reactstrap';
-import Joconde from '../../../scenes/notice/joconde';
+import React from "react";
+import { shallow } from "enzyme";
+import Joconde from "../../../scenes/notice/joconde";
+import Title from "../../../scenes/notice/components/title";
+import Field from "../../../scenes/notice/components/field";
+import sampleNotice from "../../__notices__/joconde-1";
 
-describe('Joconde suite', () => {
-  it('should work with a notice', () => {
+const placeholderParams = {
+  params: { ref: "x" },
+  isExact: true,
+  path: "",
+  url: ""
+};
+
+const headingText = wrapper => wrapper.find(".heading").text();
+
+describe("Joconde suite", () => {
+  it("should work with a simple notice", () => {
     Joconde.prototype.load = jest.fn(function(_ref) {
-      this.setState({ loading: false, notice: {TICO: 'the TICO value', IMG: []} });
+      this.setState({
+        loading: false,
+        notice: { TICO: "the TICO value", IMG: [] }
+      });
     });
-    let wrapper = shallow(<Joconde match={{params: {ref: "lorem"}, isExact: true, path: "", url: ""}} />);
-    expect(wrapper.find('.heading').text()).toContain('the TICO value')
+    let wrapper = shallow(<Joconde match={placeholderParams} />);
+    expect(headingText(wrapper)).toContain("the TICO value");
+  });
+
+  it("should work with a real notice", () => {
+    Joconde.prototype.load = jest.fn(function(_ref) {
+      this.setState({ loading: false, notice: sampleNotice });
+    });
+    let wrapper = shallow(<Joconde match={placeholderParams} />);
+    expect(headingText(wrapper)).toContain(sampleNotice.TICO);
+    expect(wrapper.find(Title)).toHaveLength(4);
+    expect(
+      wrapper
+        .find(Field)
+        .filterWhere(f => f.props().content === sampleNotice.BIBL)
+    ).toHaveLength(1);
+    expect(
+      wrapper
+        .find(Field)
+        .filterWhere(f => f.props().content === sampleNotice.LOCA)
+    ).toHaveLength(1);
+    const htmlRenderedComponent = wrapper.html();
+    expect(htmlRenderedComponent).toContain(
+      "joconde/000PE001028/96de11977.jpg"
+    );
+    expect(htmlRenderedComponent).toContain("Van Ouwenhuysen Constant");
   });
 });
