28feefd1cf2960ba0724e8e1c43697c0ad56679a
goffle <se.legoff@gmail.com>
2019-01-15 14:24:29 +0100

    add fallback to pop.reseaux@gmail.com when no addresses

--- apps/consultation/src/scenes/notice/components/ContactUs.js
+++ apps/consultation/src/scenes/notice/components/ContactUs.js
@@ -4,9 +4,10 @@ export default class ContactUs extends React.Component {
   render() {
     return (
       <a
-        href={`mailto:${
-          this.props.contact
-        }?subject=Demande à propos de la notice n°${this.props.reference}`}
+        href={`mailto:${this.props.contact ||
+          "pop.reseaux@gmail.com"}?subject=Demande à propos de la notice n°${
+          this.props.reference
+        }`}
         className="notice-btn"
       >
         Contactez-nous
