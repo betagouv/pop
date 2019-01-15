aa4df0796e3db169cd57174456227426f02d005b
Raphaël Huchet <rap2hpoutre@users.noreply.github.com>
2019-01-15 14:18:41 +0100

    Museo new data (#25)
    * prepare for new data in museo
    
    * schema

--- apps/api/src/models/museo.js
+++ apps/api/src/models/museo.js
@@ -3,39 +3,55 @@ const mongoose = require("mongoose");
 const Schema = new mongoose.Schema(
   {
     REF: { type: String, default: "", documentation: { label: "Référence" } },
-    TOUT: { type: String, default: "" },
+    TOUT: {
+      type: String,
+      default: "",
+      documentation: { label: "Index global" }
+    },
     ACCES: { type: String, default: "", documentation: { label: "Accès" } },
     ACTIV: {
       type: String,
       default: "",
       documentation: { label: "Activités culturelles" }
     },
-    ADRESSE: { type: String, default: "", documentation: { label: "Adresse" } },
-    ADRL1_M: { type: String, default: "" },
+    ADRESSE: {
+      type: String,
+      default: "",
+      documentation: { label: "Adresse complète" }
+    },
+    ADRL1_M: { type: String, default: "", documentation: { label: "Adresse" } },
     AMIS: {
       type: String,
       default: "",
-      documentation: { label: "Amis et soutiens" }
+      documentation: { label: "Amis du musée" }
     },
     AN_CREAT: {
       type: String,
       default: "",
       documentation: { label: "Année de création" }
     },
-    ANNEE_FE: { type: String, default: "" },
-    ANNEXE: { type: String, default: "" },
-    ANTARIF: { type: String, default: "" },
+    ANNEE_FE: {
+      type: String,
+      default: "",
+      documentation: { label: "Musée fermé" }
+    },
+    ANNEXE: { type: String, default: "", documentation: { label: "Annexe" } },
+    ANTARIF: {
+      type: String,
+      default: "",
+      documentation: { label: "Année tarifs" }
+    },
     ARTISTE: {
       type: String,
       default: "",
       documentation: { label: "Artistes" }
     },
-    ATOUT: {
+    ATOUT: { type: String, default: "", documentation: { label: "Atouts majeurs" } },
+    CEDEX_AD: {
       type: String,
       default: "",
-      documentation: { label: "Atouts majeurs" }
+      documentation: { label: "ADM-Cédex" }
     },
-    CEDEX_AD: { type: String, default: "" },
     COPY: {
       type: String,
       default: "",
@@ -46,7 +62,11 @@ const Schema = new mongoose.Schema(
       default: "",
       documentation: { label: "Code postal" }
     },
-    CTRLTECH: { type: String, default: "" },
+    CTRLTECH: {
+      type: String,
+      default: "",
+      documentation: { label: "Ctrl tech." }
+    },
     DOMPAL: {
       type: String,
       default: "",
@@ -66,37 +86,57 @@ const Schema = new mongoose.Schema(
     DT_SAISI: {
       type: String,
       default: "",
-      documentation: { label: "Date de saisie" }
+      documentation: { label: "Date d'entrée base" }
     },
-    GESTION: { type: String, default: "" },
-    HIST: {
+    GESTION: {
       type: String,
       default: "",
-      documentation: { label: "Historique des collections" }
+      documentation: { label: "Gestion musée" }
     },
+    HIST: { type: String, default: "", documentation: { label: "Historique" } },
     INTERET: {
       type: String,
       default: "",
-      documentation: { label: "Intérêt architectural" }
+      documentation: { label: "Intérêt arch" }
     },
-    ITI2_M: { type: String, default: "" },
-    ITI_M: { type: String, default: "" },
-    JOCONDE: {
+    ITI2_M: {
       type: String,
       default: "",
-      documentation: { label: "Base Joconde" }
+      documentation: { label: "Plan Mappy" }
     },
+    ITI_M: {
+      type: String,
+      default: "",
+      documentation: { label: "Itinéraire Mappy" }
+    },
+    JOCONDE: { type: String, default: "", documentation: { label: "Base Joconde" } },
     LABEL: { type: String, default: "", documentation: { label: "Label" } },
-    LEGS: { type: String, default: "" },
-    LIEU_M: { type: String, default: "" },
-    MEL: { type: String, default: "", documentation: { label: "Email" } },
-    MONOPLUR: { type: String, default: "" },
-    NB_AMI: { type: String, default: "" },
-    NOM_AMI: { type: String, default: "" },
+    LEGS: { type: String, default: "", documentation: { label: "Legs" } },
+    LIEU_M: {
+      type: String,
+      default: "",
+      documentation: { label: "Adresse compl." }
+    },
+    MEL: { type: String, default: "", documentation: { label: "Mél" } },
+    MONOPLUR: {
+      type: String,
+      default: "",
+      documentation: { label: "Disciplines" }
+    },
+    NB_AMI: {
+      type: String,
+      default: "",
+      documentation: { label: "Nombre amis" }
+    },
+    NOM_AMI: {
+      type: String,
+      default: "",
+      documentation: { label: "Association" }
+    },
     NOMANC: {
       type: String,
       default: "",
-      documentation: { label: "Nom ancien" }
+      documentation: { label: "Ancien nom" }
     },
     NOMOFF: {
       type: String,
@@ -106,63 +146,127 @@ const Schema = new mongoose.Schema(
     NOMUSAGE: {
       type: String,
       default: "",
-      documentation: { label: "Nom d'usage" }
+      documentation: { label: "Nom du musée" }
     },
-    OBS_AMI: { type: String, default: "" },
+    OBS_AMI: {
+      type: String,
+      default: "",
+      documentation: { label: "Ass. Amis Obs." }
+    },
     OBS_TOUR: {
       type: String,
       default: "",
       documentation: { label: "Itinéraire touristique" }
     },
-    PHARE: {
+    PHARE: { type: String, default: "", documentation: { label: "Phare" } },
+    PROPBAT: {
       type: String,
       default: "",
-      documentation: { label: "Personnages" }
+      documentation: { label: "Propr. bat." }
     },
-    PROPBAT: { type: String, default: "" },
-    PROPCOLL: { type: String, default: "" },
+    PROPCOLL: {
+      type: String,
+      default: "",
+      documentation: { label: "Propr. coll." }
+    },
     "PROT-BAT": {
       type: String,
       default: "",
-      documentation: { label: "Protection Protection bâtiment" }
+      documentation: { label: "Protect bat." }
     },
     "PROT-ESP": {
       type: String,
       default: "",
-      documentation: { label: "Protection espace" }
+      documentation: { label: "Protect esp." }
     },
-    PUBLI: { type: String, default: "" },
+    PUBLI: {
+      type: String,
+      default: "",
+      documentation: { label: "Publications" }
+    },
     REGION: { type: String, default: "", documentation: { label: "Région" } },
-    REPCOLL: { type: String, default: "" },
+    REPCOLL: {
+      type: String,
+      default: "",
+      documentation: { label: "Distr. coll" }
+    },
     SERVICES: {
       type: String,
       default: "",
       documentation: { label: "Services" }
     },
-    SIGLE_M: { type: String, default: "" },
-    STATUT: { type: String, default: "", documentation: { label: "Statut" } },
-    SURFACES: { type: String, default: "" },
-    TEL_M: { type: String, default: "", documentation: { label: "Téléphone" } },
-    THEMES: {
+    SIGLE_M: {
       type: String,
       default: "",
-      documentation: { label: "Thèmes des collections" }
+      documentation: { label: "Sigle musée" }
     },
-    URL_M2: { type: String, default: "" },
+    STATUT: {
+      type: String,
+      default: "",
+      documentation: { label: "Statut musée" }
+    },
+    SURFACES: {
+      type: String,
+      default: "",
+      documentation: { label: "Surfaces" }
+    },
+    TEL_M: { type: String, default: "", documentation: { label: "Téléphone" } },
+    THEMES: { type: String, default: "", documentation: { label: "Thèmes" } },
+    URL_M2: { type: String, default: "", documentation: { label: "Site web 2" } },
     URL_M: { type: String, default: "", documentation: { label: "Site web" } },
-    VIDEO: { type: String, default: "" },
+    VIDEO: { type: String, default: "", documentation: { label: "Image" } },
     VILLE_M: { type: String, default: "", documentation: { label: "Ville" } },
-    RESP: { type: String, default: "" },
-    GRESP: { type: String, default: "" },
-    PSC: { type: String, default: "" },
-    DPSC: { type: String, default: "" },
-    DPSC: { type: String, default: "" },
-    DMDF: { type: String, default: "" },
-    SPUB: { type: String, default: "" },
-    SPUB: { type: String, default: "" },
-    INVR: { type: String, default: "" },
-    NUMER: { type: String, default: "" },
-    LGN: { type: String, default: "" },
+    RESP: {
+      type: String,
+      default: "",
+      documentation: { label: "Resp. scient." }
+    },
+    GRESP: {
+      type: String,
+      default: "",
+      documentation: { label: "Resp. scient. HF" }
+    },
+    PSC: { type: String, default: "", documentation: { label: "Projet SC" } },
+    "DPSC-D": {
+      type: String,
+      default: "",
+      documentation: { label: "date PSC DRAC" }
+    },
+    "DPSC-S": {
+      type: String,
+      default: "",
+      documentation: { label: "date PSC SMF" }
+    },
+    DMDF: {
+      type: String,
+      default: "",
+      documentation: { label: "an appel. MDF" }
+    },
+    SPUB: {
+      type: String,
+      default: "",
+      documentation: { label: "Service publics" }
+    },
+    "SPUB-P": {
+      type: String,
+      default: "",
+      documentation: { label: "S. publics préc." }
+    },
+    INVR: {
+      type: String,
+      default: "",
+      documentation: { label: "Inventaire regl." }
+    },
+    NUMER: {
+      type: String,
+      default: "",
+      documentation: { label: "Numérisation" }
+    },
+    LGN: {
+      type: String,
+      default: "",
+      documentation: { label: "Coll. en ligne" }
+    },
     REST: { type: String, default: "" },
     ACQU: { type: String, default: "" },
     RECOL: { type: String, default: "" },
