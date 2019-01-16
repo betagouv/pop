23836a6f11f34f0dd488a4dd8a014a94875a246f
rap2h <raphaelht@gmail.com>
2019-01-16 23:22:12 +0100

    fix

--- apps/consultation/src/components/TopicCard/index.js
+++ apps/consultation/src/components/TopicCard/index.js
@@ -1,18 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Card, CardImg, CardTitle, CardBody } from "reactstrap";
+import queryString from "query-string";
 import "./index.css";
 
 class TopicCard extends React.Component {
+  searchUrl = data => {
+    const jsonData = Object.assign(
+      ...Object.entries(data).map(([k, v]) => ({ [k]: JSON.stringify(v) }))
+    );
+    const qsData = queryString.stringify(jsonData);
+    return `/search/mosaique?${qsData}`;
+  }
+
   render() {
-    const img =
-      this.props.img ||
-      "https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97270&w=318&h=270&bg=333333&txtclr=666666";
-    const txt = this.props.txt;
-    const url = this.props.url || "#";
+    const { img, txt, url } = this.props;
     return (
       <div className="topic-card">
-        <Link to={url}>
+        <Link to={this.searchUrl(url)}>
           <Card>
             <CardImg src={img} alt={txt} className="card-img" />
             <CardBody>
--- apps/consultation/src/scenes/topics/index.js
+++ apps/consultation/src/scenes/topics/index.js
@@ -1,7 +1,12 @@
 import React from "react";
 import TopicCard from "../../components/TopicCard";
 import { Row, Col, Container } from "reactstrap";
+import { bucket_url } from "../../config";
 import "./index.css";
+
+const cultureUrl = "http://www2.culture.gouv.fr/Wave/image/";
+const memoireImg = uri => `${cultureUrl}memoire/${uri}`;
+const jocondeImg = uri => `${bucket_url}joconde/${uri}`;
 
 export default () => (
   <div className="topics-view">
@@ -11,83 +16,117 @@ export default () => (
       <Row>
         <Col lg="3" md="6">
           <TopicCard
-            img="http://www2.culture.gouv.fr/Wave/image/memoire/2010/sap57_u031897vj_p.jpg"
+            img={memoireImg("2010/sap57_u031897vj_p.jpg")}
             txt="Studio Harcourt"
-            url={
-              '/search/list?base=%5B"Photographies+%28Mémoire%29"%5D&auteur=%5B"Harcourt+%28studio%29"%5D'
-            }
+            url={{
+              base: ["Photographies (Mémoire)"],
+              auteur: ["Harcourt (studio)"]
+            }}
           />
         </Col>
         <Col lg="3" md="6">
           <TopicCard
-            img="http://www2.culture.gouv.fr/Wave/image/memoire/1763/mhr91_20096630631nuca_p.jpg"
+            img={memoireImg("1763/mhr91_20096630631nuca_p.jpg")}
             txt="Josette Clier"
-            url={
-              '/search/list?base=%5B"Photographies+%28Mémoire%29"%5D&auteur=%5B"Clier%2C+Josette"%5D'
-            }
+            url={{
+              base: ["Photographies (Mémoire)"],
+              auteur: ["Clier, Josette"]
+            }}
           />
         </Col>
         <Col lg="3" md="6">
           <TopicCard
-            img="http://www2.culture.gouv.fr/Wave/image/memoire/0129/sap01_na23818917r_t.jpg"
+            img={memoireImg("0129/sap01_na23818917r_t.jpg")}
             txt="Atelier Nadar"
-            url={
-              '/search/list?base=%5B"Photographies+%28Mémoire%29"%5D&auteur=%5B"Nadar+%28atelier%29"%5D'
-            }
+            url={{
+              base: ["Photographies (Mémoire)"],
+              auteur: ["Nadar (atelier)"]
+            }}
           />
         </Col>
         <Col lg="3" md="6">
           <TopicCard
-            img="http://www2.culture.gouv.fr/Wave/image/memoire/1575/mhr93_04133996za_p.jpg"
+            img={memoireImg("1575/mhr93_04133996za_p.jpg")}
             txt="Odile de Pierrefeu"
-            url={
-              '/search/list?base=%5B"Photographies+%28Mémoire%29"%5D&auteur=%5B"de+Pierrefeu%2C+Odile"%5D'
-            }
+            url={{
+              base: ["Photographies (Mémoire)"],
+              auteur: ["de Pierrefeu, Odile"]
+            }}
           />
         </Col>
       </Row>
 
-      <h3>Peinture</h3>
+      <h3>Peinture et dessin</h3>
       <Row>
         <Col lg="3" md="6">
           <TopicCard
-            img="https://s3.eu-west-3.amazonaws.com/pop-phototeque-staging/joconde/000PE000824/88ee1903.jpg"
+            img={jocondeImg("000PE000824/88ee1903.jpg")}
             txt="Peinture à l'huile à la fin du XVIIIème siècle"
-            url={
-              'search/list?base=%5B%22Collections+des+mus%C3%A9es+de+France+%28Joconde%29%22%5D&mainSearch=%22%22&tech=%5B%22peinture+%C3%A0+l%27huile%22%5D&periode=%5B%224e+quart+18e+si%C3%A8cle%22%5D&image=%5B"oui"%5D'
-            }
+            url={{
+              base: ["Collections des musées de France (Joconde)"],
+              tech: ["peinture à l'huile"],
+              peri: ["4e quart 18e siècle"]
+            }}
           />
         </Col>
         <Col lg="3" md="6">
           <TopicCard
-            img="https://s3.eu-west-3.amazonaws.com/pop-phototeque-staging/joconde/M0759001165/e05073.jpg"
-            txt="Collections d'Asie orientale"
-            url={
-              "/search/list?base=%5B%22Collections+des+mus%C3%A9es+de+France+%28Joconde%29%22%5D&mainSearch=%22%22&results=1&image=%5B%22oui%22%5D&domn=%5B%22Asie+orientale%22%2C%22Asie+du+sud-est%22%2C%22Asie+Orientale%22%5D"
-            }
-          />
-        </Col>
-        <Col lg="3" md="6">
-          <TopicCard
-            img="https://s3.eu-west-3.amazonaws.com/pop-phototeque-staging/joconde/06650002010/0000670.jpg"
+            img={jocondeImg("06650002010/0000670.jpg")}
             txt="Les dessins d'Eugène Delacroix"
-            url={
-              "/search/list?base=%5B%22Collections+des+mus%C3%A9es+de+France+%28Joconde%29%22%5D&mainSearch=%22%22&results=1&auteur=%5B%22DELACROIX+Eug%C3%A8ne%22%5D"
-            }
+            url={{
+              base: ["Collections des musées de France (Joconde)"],
+              domn: ["dessin"],
+              auteur: ["DELACROIX Eugène"]
+            }}
           />
         </Col>
         <Col lg="3" md="6">
           <TopicCard
-            img="https://s3.eu-west-3.amazonaws.com/pop-phototeque-staging/joconde/50410000424/94-002348.jpg"
+            img={jocondeImg("50410000424/94-002348.jpg")}
             txt="Peinture sur bois de Gustave Moreau"
-            url={
-              "/search/list?base=%5B%22Collections+des+mus%C3%A9es+de+France+%28Joconde%29%22%5D&mainSearch=%22%22&auteur=%5B%22MOREAU+Gustave%22%5D&tech=%5B%22bois%22%5D"
-            }
+            url={{
+              base: ["Collections des musées de France (Joconde)"],
+              tech: ["bois"],
+              auteur: ["MOREAU Gustave"]
+            }}
+          />
+        </Col>
+
+        <Col lg="3" md="6">
+          <TopicCard
+            img={jocondeImg("11040000242/24013-4.jpg")}
+            txt="Esquisses et affiches - Alphonse Mucha"
+            url={{
+              base: ["Collections des musées de France (Joconde)"],
+              mainSearch: "mucha"
+            }}
+          />
+        </Col>
+      </Row>
+
+      <h3>Voyages</h3>
+      <Row>
+        <Col lg="3" md="6">
+          <TopicCard
+            img={jocondeImg("M0759001165/e05073.jpg")}
+            txt="Asie orientale"
+            url={{
+              base: ["Collections des musées de France (Joconde)"],
+              domn: ["Asie orientale", "Asie du sud-est", "Asie Orientale"]
+            }}
+          />
+        </Col>
+        <Col lg="3" md="6">
+          <TopicCard
+            img={jocondeImg("00000074608/rimg0445.jpg")}
+            txt="Venise"
+            url={{
+              base: ["Collections des musées de France (Joconde)"],
+              mainSearch: "venise"
+            }}
           />
         </Col>
       </Row>
     </Container>
   </div>
 );
-
-// http://localhost:8080/search/list?base=%5B%22Collections+des+mus%C3%A9es+de+France+%28Joconde%29%22%5D&mainSearch=%22%22&periode=%5B%224e+quart+18e+si%C3%A8cle%22%5D&tech=%5B%22peinture+%C3%A0+l%27huile%22%5D
