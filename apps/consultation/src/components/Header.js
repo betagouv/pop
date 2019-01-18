acc184b2579a9865abea810c669abc3083c8e7d6
goffle <se.legoff@gmail.com>
2019-01-18 11:32:40 +0100

    add button

--- apps/consultation/src/components/Header.js
+++ apps/consultation/src/components/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Container } from "reactstrap";
+import { Container, Button } from "reactstrap";
 
 import Title from "./Title.js";
 import Logo from "./Logo.js";
@@ -8,17 +8,22 @@ import Logo from "./Logo.js";
 import "./Header.css";
 
 const header = () => (
-    <div className="header">
-        <Container className="NavContainer">
-            <Logo />
-            <Title />
-            <div id="beta">
-                <div>
-                    <span>BETA</span>
-                </div>
-            </div>
-        </Container>
-    </div>
+  <div className="header">
+    <Container className="NavContainer">
+      <Logo />
+      <div className="right-container">
+        <a href="https://fier2.typeform.com/to/Qyz3xv" target="_blank">
+          <Button>Votre avis est utile</Button>
+        </a>
+        <Title />
+        <div id="beta">
+          <div>
+            <span>BETA</span>
+          </div>
+        </div>
+      </div>
+    </Container>
+  </div>
 );
 
 export default header;
