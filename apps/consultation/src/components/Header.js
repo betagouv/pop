94c7d7308c988b28fdb7f143910147f64a987c2c
goffle <se.legoff@gmail.com>
2019-01-21 10:01:14 +0100

    add button (#50)
    * add button
    
    * fix display
    
    * remove button

--- apps/consultation/src/components/Header.js
+++ apps/consultation/src/components/Header.js
@@ -8,17 +8,29 @@ import Logo from "./Logo.js";
 import "./Header.css";
 
 const header = () => (
-    <div className="header">
-        <Container className="NavContainer">
-            <Logo />
-            <Title />
-            <div id="beta">
-                <div>
-                    <span>BETA</span>
-                </div>
-            </div>
-        </Container>
-    </div>
+  <div className="header">
+    <Container className="NavContainer">
+      <Logo />
+      <div className="right-container">
+        <div>
+          <a
+            href="https://fier2.typeform.com/to/Qyz3xv"
+            class="btn btn-outline-danger d-none d-sm-block"
+            target="_blank"
+          >
+            Votre avis est utile
+          </a>
+        </div>
+
+        <Title />
+        <div id="beta">
+          <div>
+            <span>BETA</span>
+          </div>
+        </div>
+      </div>
+    </Container>
+  </div>
 );
 
 export default header;
