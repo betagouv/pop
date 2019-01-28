bc924925a6a8e6d4b4cd6e3db2bf5bd231f2e3b8
goffle <se.legoff@gmail.com>
2019-01-28 17:25:36 +0100

    hotfix

--- apps/production/src/scenes/notice/components/comments.js
+++ apps/production/src/scenes/notice/components/comments.js
@@ -5,6 +5,6 @@ export default ({ POP_COMMENTAIRES }) => {
   if (!POP_COMMENTAIRES.length) {
     return <div />;
   }
-  const comments = this.state.notice.POP_COMMENTAIRES.map(e => <div>{e}</div>);
+  const comments = POP_COMMENTAIRES.map(e => <div>{e}</div>);
   return <Alert color="danger">La notice contient des avertissements : {comments}</Alert>;
 };
