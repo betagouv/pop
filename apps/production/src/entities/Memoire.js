42a3de7572a72e89fbd576b10d9395a8af784612
goffle <se.legoff@gmail.com>
2019-01-29 16:02:37 +0100

    hotfix

--- apps/production/src/entities/Memoire.js
+++ apps/production/src/entities/Memoire.js
@@ -8,10 +8,20 @@ export default class Memoire extends Notice {
 
   extractImage(body) {
     let name = body.IMG || body.NOMSN || "";
+    name = convertLongNameToShort(name)
     if (name) {
       return `memoire/${body.REF}/${name}`;
     }
     // On retourne null car on ne veut pas overwrite les données avec ""
     return null;
   }
+}
+
+function convertLongNameToShort(str) {
+  return str
+    .substring(str.lastIndexOf("/") + 1)
+    .replace(/_[a-zA-Z0-9]\./g, ".")
+    .replace(/^.*[\\\/]/g, "")
+    .replace(/[a-zA-Z0-9]*_/g, "")
+    .toLowerCase();
 }
