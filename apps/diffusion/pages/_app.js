4bc49c967530a7d87f07262aa3f6acb27bbad2a3
rap2h <raphaelht@gmail.com>
2019-02-01 09:43:00 +0100

    fix

--- apps/diffusion/pages/_app.js
+++ apps/diffusion/pages/_app.js
@@ -4,7 +4,6 @@ import NProgress from "next-nprogress/component";
 import Router from "next/router";
 
 export default class MyApp extends App {
-
   static async getInitialProps({ Component, router, ctx }) {
     let pageProps = {};
     if (Component.getInitialProps) {
@@ -19,6 +18,13 @@ export default class MyApp extends App {
         window._paq.push(["setCustomUrl", url]);
         window._paq.push(["setDocumentTitle", document.title]);
         window._paq.push(["trackPageView"]);
+      }
+    });
+    Router.events.on("routeChangeComplete", () => {
+      if (process.env.NODE_ENV !== "production" && process.env.NODE_ENV !== "staging") {
+        const els = document.querySelectorAll('link[href*="/_next/static/css/styles.chunk.css"]');
+        const timestamp = new Date().valueOf();
+        els[0].href = "/_next/static/css/styles.chunk.css?v=" + timestamp;
       }
     });
   }
--- apps/diffusion/services/server.js
+++ apps/diffusion/services/server.js
@@ -34,8 +34,8 @@ app.prepare().then(() => {
     } else {
       handle(req, res, parsedUrl);
     }
-  }).listen(3000, err => {
+  }).listen(8081, err => {
     if (err) throw err;
-    console.log("> Ready on http://localhost:3000");
+    console.log("> Ready on http://localhost:8081");
   });
 });
