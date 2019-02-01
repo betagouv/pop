df734740b2ed89f0b5b14550da0c2af986a31266
rap2h <raphaelht@gmail.com>
2019-02-01 15:01:30 +0100

    update

--- apps/diffusion/.babelrc.js
+++ apps/diffusion/.babelrc.js
@@ -18,6 +18,7 @@ module.exports = {
   plugins: [
     "@babel/plugin-proposal-class-properties",
     "@babel/plugin-proposal-object-rest-spread",
+    "@babel/plugin-transform-block-scoping",
     ["transform-define", env]
   ]
 };
