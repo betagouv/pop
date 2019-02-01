1eb2f61adbe861433b00726940ee059e1ab527ce
rap2h <raphaelht@gmail.com>
2019-02-01 09:46:52 +0100

    hop

--- apps/diffusion/pages/_app.js
+++ apps/diffusion/pages/_app.js
@@ -4,13 +4,6 @@ import NProgress from "next-nprogress/component";
 import Router from "next/router";
 
 export default class MyApp extends App {
-  static async getInitialProps({ Component, router, ctx }) {
-    let pageProps = {};
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
-    }
-    return { pageProps };
-  }
 
   componentDidMount() {
     Router.events.on("routeChangeStart", url => {
@@ -18,13 +11,6 @@ export default class MyApp extends App {
         window._paq.push(["setCustomUrl", url]);
         window._paq.push(["setDocumentTitle", document.title]);
         window._paq.push(["trackPageView"]);
-      }
-    });
-    Router.events.on("routeChangeComplete", () => {
-      if (process.env.NODE_ENV !== "production" && process.env.NODE_ENV !== "staging") {
-        const els = document.querySelectorAll('link[href*="/_next/static/css/styles.chunk.css"]');
-        const timestamp = new Date().valueOf();
-        els[0].href = "/_next/static/css/styles.chunk.css?v=" + timestamp;
       }
     });
   }
