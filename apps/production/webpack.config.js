1af5660963a443c10827519ae97c77757d7632b3
rap2h <raphaelht@gmail.com>
2019-02-04 21:40:55 +0100

    try to avoid memory leak

--- apps/production/webpack.config.js
+++ apps/production/webpack.config.js
@@ -4,7 +4,6 @@ const webpack = require("webpack");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ManifestPlugin = require("webpack-manifest-plugin");
-const BabelPlugin = require("babel-webpack-plugin");
 
 module.exports = env => {
   const mode = env["production"] ? "production" : "staging";
@@ -31,21 +30,6 @@ module.exports = env => {
       "process.env": {
         NODE_ENV: JSON.stringify(mode)
       }
-    }),
-    new BabelPlugin({
-      test: /\.js$/,
-      presets: [
-        ['env', {
-          loose: true,
-          modules: false,
-          targets: {
-            browsers: ['>0.03%']
-          },
-          useBuiltIns: true
-        }]
-      ],
-      sourceMaps: false,
-      compact: false
     }),
     new UglifyJsPlugin({
       test: /\.js($|\?)/i,
@@ -78,9 +62,29 @@ module.exports = env => {
           test: /\.js$/,
           loader: "babel-loader",
           include: path.resolve("src"),
-          exclude: /(node_modules|__tests__)/,
-          query: {
-            babelrc: true
+          exclude: /node_modules|__tests__/,
+          options: {
+            babelrc: true,
+            cacheDirectory: true,
+            sourceMaps: false,
+            compact: false,
+            presets: [
+              [
+                "env",
+                {
+                  loose: true,
+                  modules: false,
+                  targets: {
+                    browsers: [">0.03%"]
+                  },
+                  useBuiltIns: true
+                }
+              ]
+            ],
+            plugins: [
+              "transform-class-properties",
+              "transform-object-rest-spread"
+            ]
           }
         },
         {
