6effdab896c21045eeaaeee2eac2df6b75a4de4a
goffle <se.legoff@gmail.com>
2019-02-11 13:12:04 +0100

    update

--- apps/diffusion/src/search/Search.js
+++ apps/diffusion/src/search/Search.js
@@ -9,6 +9,7 @@ import Layout from "../components/Layout";
 import Head from "next/head";
 
 import Menu from "./Menu";
+import MobileFilters from "./MobileFilters";
 import Tabs from "./Tabs";
 
 import List from "./List";
@@ -64,7 +65,7 @@ class Search extends React.Component {
                 <Menu location={this.props.location} mobile_menu={this.state.mobile_menu} />
                 <div className="search-results">
                   <Row className="search-row">
-                    <Col sm={this.props.advanced ? 10 : 6}>
+                    <Col sm={6}>
                       <div className="search-and-export-zone">
                         <DataSearch
                           componentId="mainSearch"
@@ -128,32 +129,7 @@ class Search extends React.Component {
                             };
                           }}
                         />
-                        <div
-                          className="filter_mobile_menu"
-                          onClick={() => this.setState({ mobile_menu: "mobile_open" })}
-                        >
-                          <SelectedFilters
-                            render={props => {
-                              let selectedValues = {};
-                              selectedValues = props.selectedValues;
-                              return (
-                                <div
-                                  style={{
-                                    display: "flex",
-                                    alignItems: "center"
-                                  }}
-                                >
-                                  <img src="/static/filter.png" />
-                                  <Badge color="secondary">
-                                    {Object.keys(selectedValues).reduce((acc, current) => {
-                                      return selectedValues[current].value !== "" ? acc + 1 : acc;
-                                    }, 0)}
-                                  </Badge>
-                                </div>
-                              );
-                            }}
-                          />
-                        </div>
+                        <MobileFilters mobile_menu={this.state.mobile_menu} />
                       </div>
                     </Col>
                     <Col sm={2} className="advanced">
