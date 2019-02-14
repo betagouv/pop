66b0ec4b6543af702cda14d48813a2a0cbfecb09
Raphaël Huchet <rap2hpoutre@users.noreply.github.com>
2019-02-14 14:33:40 +0100

    toggle result view (#138)

--- apps/diffusion/src/search/Results/Results.js
+++ apps/diffusion/src/search/Results/Results.js
@@ -1,9 +1,9 @@
 import React from "react";
-import Link from "next/link";
-
+import Router from "next/router";
 import Map from "./Map";
 import Mosaic from "./Mosaic";
 import List from "./List";
+import queryString from "query-string";
 
 const DEFAULT_FILTER = [
   "mainSearch",
@@ -25,24 +25,64 @@ const DEFAULT_FILTER = [
 ];
 
 class Results extends React.Component {
+  toggle(subRoute, params) {
+    if (this.props.display !== subRoute) {
+      const JsonStringifyValues = object => {
+        const entries = Object.entries(object)
+          .filter(([_k, v]) => (Array.isArray(v) ? v.length : v))
+          .map(([k, v]) => v && { [k]: JSON.stringify(v) });
+        return entries.length ? Object.assign(...entries) : null;
+      };
+      const JsonParseValues = values => {
+        const entries =
+          values &&
+          Object.entries(values).map(([k, v]) => {
+            try {
+              v = JSON.parse(v);
+            } catch (e) {}
+            return { [k]: v };
+          });
+        return entries.length ? Object.assign(...entries) : {};
+      };
+      const search = JsonStringifyValues({
+        ...JsonParseValues(queryString.parse(window.location.search)),
+        ...params
+      });
+      Router.push(`/search/${subRoute}${search ? "?" + queryString.stringify(search) : ""}`);
+    }
+  }
+
   renderTabs() {
     const view = this.props.view || "list";
     return (
       <ul className="nav nav-pills">
         <li className="nav-item">
-          <Link href="/search/list">
-            <a className={`${view === "list" ? "active " : ""} nav-link`}>LISTE</a>
-          </Link>
+          <a
+            onClick={() => this.toggle("list", { geolocalisation: [], image: [] })}
+            className={`${view === "list" ? "active " : ""} nav-link`}
+          >
+            LISTE
+          </a>
         </li>
         <li className="nav-item">
-          <Link href='/search/map?geolocalisation=%5B"oui"%5D'>
-            <a className={`${view === "map" ? "active " : ""} nav-link`}>CARTE</a>
-          </Link>
+          <a
+            onClick={() => {
+              this.toggle("map", { geolocalisation: ["oui"], image: [] });
+            }}
+            className={`${view === "map" ? "active " : ""} nav-link`}
+          >
+            CARTE
+          </a>
         </li>
         <li className="nav-item">
-          <Link href='/search/mosaic?image=%5B"oui"%5D'>
-            <a className={`${view === "mosaic" ? "active " : ""} nav-link`}>MOSAIQUE</a>
-          </Link>
+          <a
+            onClick={() => {
+              this.toggle("mosaic", { image: ["oui"], geolocalisation: [] });
+            }}
+            className={`${view === "mosaic" ? "active " : ""} nav-link`}
+          >
+            MOSAIQUE
+          </a>
         </li>
       </ul>
     );
