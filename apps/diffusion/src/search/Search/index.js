977e3da33373c2d561c170e62c1ebf044291b30c
rap2h <raphaelht@gmail.com>
2019-02-15 11:55:11 +0100

    fix multilist before advanced search

--- apps/diffusion/src/search/Search/index.js
+++ apps/diffusion/src/search/Search/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import Link from "next/link";
-import queryString from "query-string";
-
 import TextSearch from "./Search";
 import AdvancedSearch from "./SearchAdvanced";
 
