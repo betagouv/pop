ef0a788902cfc7a69201e52aedac04dc332dd319
rap2h <raphaelht@gmail.com>
2019-02-18 10:58:45 +0100

    fix prefetch

--- apps/diffusion/pages/index.js
+++ apps/diffusion/pages/index.js
@@ -87,7 +87,7 @@ const bases = [
 
 export default class extends React.Component {
   componentDidMount() {
-    Router.prefetch("/search/list");
+    Router.prefetch("/search");
   }
   constructor(props) {
     super(props);
