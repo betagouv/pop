f6a9012787b11ada0c8c9016e603a243ef53e564
rap2h <raphaelht@gmail.com>
2019-02-21 10:59:37 +0100

    polyfill

--- apps/diffusion/.babelrc.js
+++ apps/diffusion/.babelrc.js
@@ -9,7 +9,7 @@ module.exports = {
           targets: {
             browsers: [">0.03%"]
           },
-          useBuiltIns: "usage"
+          useBuiltIns: "entry"
         }
       }
     ]
--- apps/diffusion/next.config.js
+++ apps/diffusion/next.config.js
@@ -1,2 +1,16 @@
 const withCSS = require("@zeit/next-css");
-module.exports = withCSS();
+module.exports = withCSS({
+  webpack: config => {
+    // Unshift polyfills in main entrypoint.
+    // Source: https://github.com/zeit/next.js/issues/2060#issuecomment-385199026
+    const originalEntry = config.entry;
+    config.entry = async () => {
+      const entries = await originalEntry();
+      if (entries["main.js"]) {
+        entries["main.js"].unshift("./src/polyfill.js");
+      }
+      return entries;
+    };
+    return config;
+  }
+});
