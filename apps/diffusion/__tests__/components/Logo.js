4394ef53cc4d5600e0f02aef54efaf797f516bff
rap2h <raphaelht@gmail.com>
2019-02-21 10:35:33 +0100

    remove tests

--- apps/diffusion/__tests__/components/Logo.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import Logo from '../../src/components/Logo';
-
-describe('Logo suite', function() {
-  it('should have exactly one Link', () => {
-    expect(shallow(<Logo />).find(".logo")).toHaveLength(1)
-  });
-})
--- apps/diffusion/__tests__/components/Title.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import Title from '../../src/components/Title';
-
-describe('Title suite', function() {
-  it('should contain "Patrimoine"', () => {
-    expect(shallow(<Title />).text()).toContain('Patrimoine')
-  });
-})
