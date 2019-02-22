82eae8160e1562c730e766683534831a789cf2a6
rap2h <raphaelht@gmail.com>
2019-02-22 11:24:03 +0100

    add muse update route

--- apps/api/src/controllers/museo.js
+++ apps/api/src/controllers/museo.js
@@ -1,18 +1,36 @@
 const express = require("express");
 const router = express.Router();
+const bodyParser = require("body-parser");
+const passport = require("passport");
 const Museo = require("../models/museo");
 const { formattedNow } = require("./utils");
 
-function transformBeforeCreateOrUpdate(notice) {
-  notice.DT_MODIF = formattedNow();
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json());
+
+function transformBeforeCreateOrUpdate(museo) {
+  museo.DT_MODIF = formattedNow();
 }
 
-router.use("/:ref", async (req, res) => {
+router.get("/:ref", async (req, res) => {
   const museo = await Museo.findOne({ REF: req.params.ref });
   if (museo) {
     return res.status(200).send(museo);
   }
   return res.sendStatus(404);
+});
+
+router.put("/:ref", passport.authenticate("jwt", { session: false }), async (req, res) => {
+  const data = JSON.parse(req.body.museo);
+  if (!data || !data.REF) {
+    return res.status(400).json({ success: false, msg: "Objet museo ou référence absente" });
+  }
+  transformBeforeCreateOrUpdate(data);
+  const doc = await Museo.findOneAndUpdate({ REF: data.REF }, data, { new: true });
+  if (!doc) {
+    return res.sendStatus(404);
+  }
+  return res.status(200).send(doc);
 });
 
 module.exports = router;
