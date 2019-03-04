0972c909b7be6ca002c8752d0369edbb28aef180
goffle <se.legoff@gmail.com>
2019-03-04 14:39:37 +0100

    remove window

--- apps/diffusion/src/services/amplitude.js
+++ apps/diffusion/src/services/amplitude.js
@@ -1,16 +1,16 @@
 const amplitudeService = {};
 amplitudeService.init = function() {
-  if (process.env.NODE_ENV === "production" && window.amplitude) {
-    window.amplitude.getInstance().init("6e94a0dbfe3dec83eb4adab5088fc084");
+  if (process.env.NODE_ENV === "production" && amplitude) {
+    amplitude.getInstance().init("6e94a0dbfe3dec83eb4adab5088fc084");
   }
 };
 
 amplitudeService.logEvent = function(eventName, properties = {}) {
   console.log("TRACK", eventName, properties);
-  console.log("TRACKED", process.env.NODE_ENV, window.amplitude);
-  if (process.env.NODE_ENV === "production" && window.amplitude) {
+  console.log("TRACKED", process.env.NODE_ENV);
+  if (process.env.NODE_ENV === "production" && amplitude) {
     console.log("OK");
-    window.amplitude.getInstance().logEvent(eventName, properties);
+    amplitude.getInstance().logEvent(eventName, properties);
   }
 };
 
