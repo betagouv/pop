84020eb56c15bbd8dc43c4c4d57a327fa6b57001
rap2h <raphaelht@gmail.com>
2019-03-05 10:21:01 +0100

    init

---
+++ apps/api/src/models/enluminures.js
@@ -0,0 +1,71 @@
+const mongoose = require("mongoose");
+var mongoosastic = require("mongoosastic");
+var getElasticInstance = require("../elasticsearch");
+
+const Schema = new mongoose.Schema(
+  {
+    REF: {
+      type: String,
+      unique: true,
+      index: true,
+      trim: true,
+      required: true,
+      documentation: { label: "Référence" }
+    },
+    ATTRIB: { type: String, default: "" },
+    APPL: { type: String, default: "" },
+    AUTR: { type: String, default: "" },
+    AUTS: { type: String, default: "" },
+    CONSERV: { type: String, default: "" },
+    CONTXT: { type: String, default: "" },
+    COTE: { type: String, default: "" },
+    DATE: { type: String, default: "" },
+    DATDEB: { type: String, default: "" },
+    DATFIN: { type: String, default: "" },
+    DIMS: { type: String, default: "" },
+    ETAB: { type: String, default: "" },
+    FOPG: { type: String, default: "" },
+    FOLIOS: { type: String, default: "" },
+    LANGOUV: { type: String, default: "" },
+    NFICH: { type: String, default: "" },
+    NVUE: { type: String, default: "" },
+    NOMENC: { type: String, default: "" },
+    NOTES: { type: String, default: "" },
+    NOTDEC: { type: String, default: "" },
+    OPHOT: { type: String, default: "" },
+    ORIGG: { type: String, default: "" },
+    ORIGH: { type: String, default: "" },
+    ORIGP: { type: String, default: "" },
+    DOMN: { type: String, default: "" },
+    TYPE: { type: String, default: "" },
+    POSS: { type: String, default: "" },
+    REFD: { type: String, default: "" },
+    REFIM: { type: String, default: "" },
+    ENRGFP: { type: String, default: "" },
+    ENRGMS: { type: String, default: "" },
+    DROIT: { type: String, default: "" },
+    COPY: { type: String, default: "" },
+    SUJET: { type: String, default: "" },
+    SUPP: { type: String, default: "" },
+    TITR: { type: String, default: "" },
+    TYPDEC: { type: String, default: "" },
+    TYPCOD: { type: String, default: "" },
+    LOCA: { type: String, default: "" },
+    LOCA2: { type: String, default: "" },
+    VISITE: { type: String, default: "" },
+    VIDEO: { type: String, default: "" },
+    TOUT: { type: String, default: "" },
+    IMG: { type: String, default: "" }
+  },
+  { collection: "enluminures" }
+);
+
+Schema.plugin(mongoosastic, {
+  esClient: getElasticInstance(),
+  index: "enluminures",
+  bulk: { size: 500, delay: 2000 }
+});
+
+const object = mongoose.model("enluminures", Schema);
+
+module.exports = object;
---
+++ apps/api/src/scripts/import-enluminures.js
@@ -0,0 +1,28 @@
+const parse = require("csv-parse/lib/sync");
+const fs = require("fs");
+const { mongoUrl } = require("../config.js");
+const mongoose = require("mongoose");
+const Enluminures = require("../models/enluminures");
+
+async function run() {
+  mongoose.set("useCreateIndex", true);
+  await mongoose.connect(mongoUrl, { useNewUrlParser: true });
+
+  const r = parse(
+    fs.readFileSync("/Users/raphael/Downloads/baseEnluminures/enlumine-DECOR-valid.csv.utf.csv"),
+    { columns: true, skip_empty_lines: true, delimiter: "|", quote: false }
+  );
+
+  const total = r.length;
+  for (let i in r) {
+    const e = r[i];
+    e.REF = e.REF.trim();
+    await Enluminures.findOneAndUpdate({ REF: e.REF }, e, { upsert: true, new: true });
+    if (i % 500 === 0) {
+      console.log((i / total) * 100 + "%");
+    }
+  }
+  console.log("done");
+}
+
+run();
