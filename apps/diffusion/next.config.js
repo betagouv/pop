94e62a9cf902d48b3694cf56ec0a533fe8f53d59
rap2h <raphaelht@gmail.com>
2019-03-12 16:39:31 +0100

    service worker

--- apps/diffusion/next.config.js
+++ apps/diffusion/next.config.js
@@ -1,4 +1,6 @@
 const withCSS = require("@zeit/next-css");
+const SWPrecacheWebpackPlugin = require("sw-precache-webpack-plugin");
+
 module.exports = withCSS({
   webpack: config => {
     // Unshift polyfills in main entrypoint.
@@ -11,6 +13,20 @@ module.exports = withCSS({
       }
       return entries;
     };
+    // Source: https://github.com/zeit/next.js/tree/85769c3d3296cdcddc0fb36f05058c8e451ca57f/examples/with-sw-precache
+    config.plugins.push(
+      new SWPrecacheWebpackPlugin({
+        verbose: true,
+        staticFileGlobsIgnorePatterns: [/\.next\//],
+        runtimeCaching: [
+          {
+            handler: "networkFirst",
+            urlPattern: /^https?.*/
+          }
+        ]
+      })
+    );
+
     return config;
   }
 });
