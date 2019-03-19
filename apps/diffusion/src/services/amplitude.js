8a775411ff869d980c71f3281c36c1c342c80a8d
rap2h <raphaelht@gmail.com>
2019-03-19 13:45:28 +0100

    hop

--- apps/diffusion/src/services/amplitude.js
+++ apps/diffusion/src/services/amplitude.js
@@ -1,3 +1,3 @@
 export default function logEvent (event, props = {}) {
-  window && window.amplitude && amplitude.getInstance().logEvent(event, props);
+  window && window.amplitude && window.amplitude.getInstance().logEvent(event, props);
 }
