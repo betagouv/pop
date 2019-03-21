c6d80f2eae569bd63e468f70cb424e5514df6c53
goffle <se.legoff@gmail.com>
2019-03-21 10:39:28 +0100

    force https on production (#220)

--- apps/production/src/server/index.js
+++ apps/production/src/server/index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const path = require("path");
 const hsts = require("hsts");
+const forceDomain = require("forcedomain");
 
 const app = express();
 const port = 8081;
 
 console.log("START", new Date());
+
+app.use(forceDomain({ hostname: "production.pop.culture.gouv.fr", protocol: "https" }));
+
+app.use(hsts({ maxAge: 31536000, includeSubDomains: true, preload: true }));
 
 app.use(express.static(path.join(__dirname, "/../../build")));
 
