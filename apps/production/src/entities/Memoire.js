79d742e55d27690c8bff26d9c18a5e5ced13fbe2
goffle <se.legoff@gmail.com>
2019-03-21 15:15:40 +0100

    for uppercase on reference

--- apps/production/src/entities/Memoire.js
+++ apps/production/src/entities/Memoire.js
@@ -2,6 +2,9 @@ import Notice from "./Notice";
 
 export default class Memoire extends Notice {
   constructor(body) {
+    if (body.REF) {
+      body.REF = body.REF.toUpperCase();
+    }
     super(body, "memoire");
   }
 
