3598d25e056cabe15357c32d011d34f99aa19b72
rap2h <raphaelht@gmail.com>
2019-04-02 11:41:17 +0200

    switch search

--- apps/diffusion/src/search/Search/index.js
+++ apps/diffusion/src/search/Search/index.js
@@ -1,42 +1,14 @@
 import React from "react";
-import Link from "next/link";
 import TextSearch from "./Search";
 import AdvancedSearch from "./SearchAdvanced";
 
 class Search extends React.Component {
-  renderSearch() {
+  render() {
     if (this.props.mode !== "advanced") {
       return <TextSearch />;
     } else {
       return <AdvancedSearch base={this.props.base} />;
     }
-  }
-
-  renderLink() {
-    if (this.props.mode !== "advanced") {
-      return (
-        <Link
-          href={"/search?view=list&mode=advanced&base=joconde"}
-          as="/advanced-search/list/joconde"
-        >
-          <a className="search-mode">Recherche avancée</a>
-        </Link>
-      );
-    }
-    return (
-      <Link href={"/search?view=list&mode=simple"} as="/search/list">
-        <a className="search-mode">Recherche simple</a>
-      </Link>
-    );
-  }
-
-  render() {
-    return (
-      <React.Fragment>
-        {this.renderSearch()}
-        <div className="switch-search">{this.renderLink()}</div>
-      </React.Fragment>
-    );
   }
 }
 
