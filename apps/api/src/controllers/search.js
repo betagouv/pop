d918aa0691584f65e17f16b350971a7d08fb6e66
rap2h <raphaelht@gmail.com>
2019-04-03 16:37:46 +0200

    fix

--- apps/api/src/controllers/search.js
+++ apps/api/src/controllers/search.js
@@ -8,13 +8,13 @@ const aws4 = require("aws4");
 // https://www.elastic.co/guide/en/elasticsearch/reference/current/search-request-scroll.html#search-request-scroll
 router.post("/scroll", (req, res) => {
   let path, body;
+  // First request with a full query, next with a scroll_id.
   if (req.query.scroll_id) {
     path = "/_search/scroll";
     body = JSON.stringify({ scroll: "1m", scroll_id: req.query.scroll_id });
   } else if (req.query.index && req.query.index.match(/^[a-z]+$/)) {
     path = `/${req.query.index}/_search?scroll=1m`;
     body = req.body;
-    console.log(req.body, typeof req.body);
   } else {
     return res.sendStatus(400);
   }
