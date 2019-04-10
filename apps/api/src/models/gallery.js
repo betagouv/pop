61c644962693f0030f513b4ada909a146fae5efa
goffle <se.legoff@gmail.com>
2019-04-10 09:24:54 +0200

    up

--- apps/api/src/models/gallery.js
+++ apps/api/src/models/gallery.js
@@ -1,7 +1,20 @@
 const mongoose = require("mongoose");
+var mongoosastic = require("mongoosastic");
+var getElasticInstance = require("../elasticsearch");
 
-const GallerySchema = new mongoose.Schema({
-  params: {}
-}, { collection: "gallery" });
+const GallerySchema = new mongoose.Schema(
+  {
+    params: {},
+    createdBy: String,
+    createdAt: String,
+    name: String
+  },
+  { collection: "gallery" }
+);
+
+GallerySchema.plugin(mongoosastic, {
+  esClient: getElasticInstance(),
+  index: "gallery"
+});
 
 module.exports = mongoose.model("Gallery", GallerySchema);
