cb0178a3135fdb30a6c6b75a63be8d66bf32e622
rap2h <raphaelht@gmail.com>
2019-05-06 14:59:15 +0200

    revert

--- apps/production/src/scenes/gallery/index.js
+++ apps/production/src/scenes/gallery/index.js
@@ -1,76 +1,86 @@
 import React from "react";
 import {
-  Elasticsearch,
-  SearchBox,
-  Results,
-  toUrlQueryString,
-  fromUrlQueryString,
-  ActiveFilters
-} from "react-elasticsearch";
+  ReactiveBase,
+  DataSearch,
+  ReactiveList,
+  SelectedFilters,
+  MultiList
+} from "@appbaseio/reactivesearch/lib";
 import { Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import { Row, Col } from "reactstrap";
-import utils from "../search/components/utils";
-import CollapsableFacet from "../search/components/CollapsableFacet";
+
 import "./index.css";
 
 import { es_url, bucket_url } from "../../config.js";
 
-// TODO: fix
+const FILTER = ["mainSearch", "institution", "email", "notices"];
+
 class Gallery extends React.Component {
   render() {
     // Only admin can view galleries.
     if (!(this.props.role === "administrateur" && this.props.group === "admin")) {
       return <Redirect to="/recherche" />;
     }
-    const initialValues = fromUrlQueryString(window.location.search.replace(/^\?/, ""));
     return (
       <div className="list-gallery">
-        <Elasticsearch
-          url={`${es_url}/gallery`}
-          onChange={params => {
-            const q = toUrlQueryString(params);
-            if (q) {
-              window.history.replaceState("x", "y", `?${q}`);
-            }
-          }}
-        >
-          <div>
-            <SearchBox
-              id="main"
-              placeholder="Saisissez un nom ou un email"
-              initialValue={initialValues.get("main")}
-              customQuery={value => utils.customQuery(value, ["name", "createdBy"])}
-            />
-          </div>
+        <ReactiveBase url={`${es_url}/gallery`} app="gallery">
           <Row>
-            <Col xs="3">
-              <CollapsableFacet
-                initialCollapsed={false}
-                id="institution"
-                initialValue={initialValues.get("institution")}
-                fields={["institution.keyword"]}
-                title="Institutions"
+            <Col md="3">
+              <SelectedFilters clearAllLabel="Tout supprimer" />
+              <DataSearch
+                className="filter"
+                componentId="mainSearch"
+                dataField={["name", "email"]}
+                queryFormat="and"
+                iconPosition="left"
+                title="Recherche"
+                className="mainSearch"
+                placeholder="Saisissez un nom ou un email"
+                URLParams={true}
               />
-              <CollapsableFacet
-                initialCollapsed={false}
-                id="email"
-                initialValue={initialValues.get("email")}
-                fields={["createdBy.keyword"]}
-                title="Créé par"
+              <MultiList
+                className="filter"
+                componentId="institution"
+                dataField="institution.keyword"
+                title="Institutions"
+                placeholder="Sélectionnez une institution"
+                react={{ and: FILTER.filter(e => e !== "institution") }}
+              />
+              <MultiList
+                className="filter"
+                componentId="email"
+                dataField="createdBy.keyword"
+                title="Créé par "
+                placeholder="Sélectionnez un email"
+                react={{ and: FILTER.filter(e => e !== "email") }}
               />
             </Col>
-            <Col xs="9">
-              <ActiveFilters id="af" />
-              <Results
-                initialPage={initialValues.get("resPage")}
-                id="res"
-                item={(source, _score, id) => <Card key={id} data={source} />}
-                pagination={utils.pagination}
+            <Col md="9">
+              <ReactiveList
+                sortOptions={[
+                  { label: "Les plus récents en premier", dataField: "createdAt", sortBy: "desc" },
+                  { label: "Les plus anciens en premier", dataField: "createdAt", sortBy: "asc" }
+                ]}
+                componentId="results"
+                className="reactive-list"
+                react={{ and: FILTER }}
+                onResultStats={(total, took) => {
+                  if (total === 1) {
+                    return `1 résultat`;
+                  }
+                  return `${total} résultats`;
+                }}
+                onNoResults="Aucun résultat trouvé."
+                loader="Préparation de l'affichage des résultats..."
+                URLParams={true}
+                dataField=""
+                size={20}
+                onData={data => <Card key={data._id} data={data} />}
               />
             </Col>
           </Row>
-        </Elasticsearch>
+        </ReactiveBase>
       </div>
     );
   }
