1b9240f4696afdbb3f2fb8d089d2f121b2cf46b9
rap2h <raphaelht@gmail.com>
2019-05-16 16:24:48 +0200

    scroll top

--- apps/production/src/redux/reducers.js
+++ apps/production/src/redux/reducers.js
@@ -1,9 +1,13 @@
 import { reducer as form } from "redux-form";
 import { reducer as toastr } from "react-redux-toastr";
 import Auth from "./auth/reducer";
+import { combineReducers } from "redux";
+import { connectRouter } from "connected-react-router";
 
-export default {
-  form,
-  toastr,
-  Auth
-};
+export default history =>
+  combineReducers({
+    router: connectRouter(history),
+    form,
+    toastr,
+    Auth
+  });
--- apps/production/src/router.js
+++ apps/production/src/router.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Redirect, Switch } from "react-router-dom";
-import { ConnectedRouter } from "react-router-redux";
+import { ConnectedRouter } from "connected-react-router";
 import "react-redux-toastr/lib/css/react-redux-toastr.min.css";
 import ReduxToastr from "react-redux-toastr";
 import { connect } from "react-redux";
@@ -15,76 +15,79 @@ import Import from "./scenes/import";
 import Gallery from "./scenes/gallery";
 import Thesaurus from "./scenes/thesaurus";
 import Auth from "./scenes/auth";
+import ScrollTop from "./components/ScrollTop";
 
 class PublicRoutes extends React.Component {
   render() {
     return (
       <ConnectedRouter history={this.props.history}>
-        <div className="main">
-          <ReduxToastr
-            timeOut={2000}
-            newestOnTop={false}
-            preventDuplicates
-            position="top-right"
-            transitionIn="fadeIn"
-            transitionOut="fadeOut"
-            confirmOptions={{ okText: "Oui", cancelText: "Annuler" }}
-          />
-          <Header />
-          <Switch>
-            <RestrictedRoute
-              exact
-              path={"/"}
-              component={Home}
-              isLoggedIn={this.props.isLoggedIn}
-              hasResetPassword={this.props.hasResetPassword}
+        <ScrollTop>
+          <div className="main">
+            <ReduxToastr
+              timeOut={2000}
+              newestOnTop={false}
+              preventDuplicates
+              position="top-right"
+              transitionIn="fadeIn"
+              transitionOut="fadeOut"
+              confirmOptions={{ okText: "Oui", cancelText: "Annuler" }}
             />
-            <Route path={"/auth/"} component={Auth} />
-            <Route
-              exact
-              path={"/thesaurus"}
-              component={Thesaurus}
-              isLoggedIn={this.props.isLoggedIn}
-              hasResetPassword={this.props.hasResetPassword}
-            />
-            <RestrictedRoute
-              path={"/recherche/"}
-              component={SimpleSearch}
-              isLoggedIn={this.props.isLoggedIn}
-              hasResetPassword={this.props.hasResetPassword}
-            />
-            <RestrictedRoute
-              path={"/recherche-avancee/"}
-              component={AdvancedSearch}
-              isLoggedIn={this.props.isLoggedIn}
-              hasResetPassword={this.props.hasResetPassword}
-            />
-            <RestrictedRoute
-              path={"/admin/"}
-              component={Admin}
-              isLoggedIn={this.props.isLoggedIn}
-              hasResetPassword={this.props.hasResetPassword}
-            />
-            <RestrictedRoute
-              path={"/import/"}
-              component={Import}
-              isLoggedIn={this.props.isLoggedIn}
-              hasResetPassword={this.props.hasResetPassword}
-            />
-            <RestrictedRoute
-              path={"/gallery/"}
-              component={Gallery}
-              isLoggedIn={this.props.isLoggedIn}
-              hasResetPassword={this.props.hasResetPassword}
-            />
-            <RestrictedRoute
-              path={"/notice/:collection/:ref"}
-              component={Notice}
-              isLoggedIn={this.props.isLoggedIn}
-              hasResetPassword={this.props.hasResetPassword}
-            />
-          </Switch>
-        </div>
+            <Header />
+            <Switch>
+              <RestrictedRoute
+                exact
+                path={"/"}
+                component={Home}
+                isLoggedIn={this.props.isLoggedIn}
+                hasResetPassword={this.props.hasResetPassword}
+              />
+              <Route path={"/auth/"} component={Auth} />
+              <Route
+                exact
+                path={"/thesaurus"}
+                component={Thesaurus}
+                isLoggedIn={this.props.isLoggedIn}
+                hasResetPassword={this.props.hasResetPassword}
+              />
+              <RestrictedRoute
+                path={"/recherche/"}
+                component={SimpleSearch}
+                isLoggedIn={this.props.isLoggedIn}
+                hasResetPassword={this.props.hasResetPassword}
+              />
+              <RestrictedRoute
+                path={"/recherche-avancee/"}
+                component={AdvancedSearch}
+                isLoggedIn={this.props.isLoggedIn}
+                hasResetPassword={this.props.hasResetPassword}
+              />
+              <RestrictedRoute
+                path={"/admin/"}
+                component={Admin}
+                isLoggedIn={this.props.isLoggedIn}
+                hasResetPassword={this.props.hasResetPassword}
+              />
+              <RestrictedRoute
+                path={"/import/"}
+                component={Import}
+                isLoggedIn={this.props.isLoggedIn}
+                hasResetPassword={this.props.hasResetPassword}
+              />
+              <RestrictedRoute
+                path={"/gallery/"}
+                component={Gallery}
+                isLoggedIn={this.props.isLoggedIn}
+                hasResetPassword={this.props.hasResetPassword}
+              />
+              <RestrictedRoute
+                path={"/notice/:collection/:ref"}
+                component={Notice}
+                isLoggedIn={this.props.isLoggedIn}
+                hasResetPassword={this.props.hasResetPassword}
+              />
+            </Switch>
+          </div>
+        </ScrollTop>
       </ConnectedRouter>
     );
   }
