fff517093d4496c03c4ac267603984e1d141e5a8
goffle <se.legoff@gmail.com>
2019-05-24 17:34:14 +0200

    update museo (#307)

--- apps/production/src/scenes/search/components/MuseoCard.js
+++ apps/production/src/scenes/search/components/MuseoCard.js
@@ -1,22 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { bucket_url } from "../../../config.js";
 
 export default ({ data }) => {
+  const image = data.PHOTO ? `${bucket_url}${data.PHOTO}` : require("../../../assets/noimage.jpg");
   return (
-    <Link
-      style={{ textDecoration: "none" }}
-      className="card"
-      to={`/notice/museo/${data.REF}`}
-    >
+    <Link style={{ textDecoration: "none" }} className="card" to={`/notice/museo/${data.REF}`}>
+      <img src={image} alt="Lien cassé" />
       <div className="content">
         <div style={{ display: "flex" }}>
-          <h2>{data.NOMOFF}</h2>
+          <h2>{data.NOMUSAGE || data.NOMOFF}</h2>
           <span>{data.REF}</span>
         </div>
         <div>
-          <p>{data.THEMES}</p>
-          <p>{data.NOMOFF}</p>
-          <p>{data.INV}</p>
+          <p>
+            {data.VILLE_M} ({data.DPT}, {data.REGION})
+          </p>
+          <p>{data.CATEG}</p>
+          <p>{data.DOMPAL}</p>
         </div>
       </div>
     </Link>
