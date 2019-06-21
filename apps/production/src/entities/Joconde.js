14e938d644bc59306c25c995f6641760311816d3
goffle <se.legoff@gmail.com>
2019-06-21 14:39:23 +0200

    up LOCA stuff when you import

--- apps/production/src/entities/Joconde.js
+++ apps/production/src/entities/Joconde.js
@@ -9,6 +9,13 @@ export default class Joconde extends Notice {
       this.IMG = this.extractIMGNames(body.REFIM).map(e => `joconde/${this.REF}/${e}`);
     }
 
+    if (body.LOCA) {
+      const { MANQUANT, MANQUANT_COM } = this.extractManquant(body.LOCA);
+      this.MANQUANT = MANQUANT;
+      this.MANQUANT_COM = MANQUANT_COM;
+    }
+
+    //ADD Boring stuff in order to split text to check for the thesaurus
     const obj = {};
     obj["AUTR"] = /[(,);#]/g;
     obj["DECV"] = /[(,);#]/g;
@@ -31,9 +38,31 @@ export default class Joconde extends Notice {
     for (let key in obj) {
       this._mapping[key].thesaurus_separator = obj[key];
     }
-
-    //REFMIS ?
   }
+
+  extractManquant = function(str) {
+    const MANQUANT = "manquant";
+
+    if (["détruit", "détruite", "detruit", "detruite"].some(e => str.indexOf(e) !== -1)) {
+      return { MANQUANT: "", MANQUANT_COM: "détruit" };
+    }
+    if (["localisation inconnue"].some(e => str.indexOf(e) !== -1)) {
+      return { MANQUANT, MANQUANT_COM: "" };
+    }
+    if (["disparu", "disparue", "disparus"].some(e => str.indexOf(e) !== -1)) {
+      return { MANQUANT, MANQUANT_COM: "disparu" };
+    }
+    if (["pillé", "pillée"].some(e => str.indexOf(e) !== -1)) {
+      return { MANQUANT, MANQUANT_COM: "pillé" };
+    }
+    if (["volé", "volée", "volés", "volées"].some(e => str.indexOf(e) !== -1)) {
+      return { MANQUANT, MANQUANT_COM: "volé" };
+    }
+    if (["manquant", "manquants", "manquante", "manquantes"].some(e => str.indexOf(e) !== -1)) {
+      return { MANQUANT, MANQUANT_COM: "" };
+    }
+    return { MANQUANT: "", MANQUANT_COM: "" };
+  };
 
   extractIMGNames = function(str) {
     if (!str) {
