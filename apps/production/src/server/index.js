ce6377bc8ee837111ed5b0a01012288621bafa30
Raphaël Huchet <rap2hpoutre@users.noreply.github.com>
2019-06-26 11:09:54 +0200

    force https (#370)

--- apps/production/src/server/index.js
+++ apps/production/src/server/index.js
@@ -1,23 +1,26 @@
 const express = require("express");
 const path = require("path");
 const hsts = require("hsts");
-const forceDomain = require("forcedomain");
+
+console.log("START", new Date());
 
 const app = express();
 const port = 8081;
 
-console.log("START", new Date());
+function forceHttps(res, req, next) {
+  const isProdDomain = req.get("host").match(/production\.pop\.culture\.gouv\.fr/);
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && isProdDomain) {
+    return res.redirect(302, 'https://production.pop.culture.gouv.fr' + req.url);
+  }
+  next();
+}
 
-// app.use(forceDomain({ hostname: "production.pop.culture.gouv.fr", protocol: "https" }));
-
+app.use(forceHttps);
 app.use(hsts({ maxAge: 31536000, includeSubDomains: true, preload: true }));
-
 app.use(express.static(path.join(__dirname, "/../../build")));
-
 app.route("*").all((req, res) => {
   res.sendFile(path.join(__dirname, "/../../build/index.html"));
 });
-
 app.listen(port, () => {
   console.log(`App listening at port:${port}`);
 });
