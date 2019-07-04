cfe616adaeebfc0ebf6bdd79c20c48e746a5366b
goffle <se.legoff@gmail.com>
2019-07-04 16:25:39 +0200

    up

--- apps/production/src/scenes/import/joconde.js
+++ apps/production/src/scenes/import/joconde.js
@@ -327,6 +327,10 @@ function readme() {
           Charte d'engagement
         </a>{" "}
         relative au versement des données dans la base Joconde <br /> <br />
+        <h6>Exemple de fichier d'import csv</h6>
+        <a href="https://pop-general.s3.eu-west-3.amazonaws.com/import_examples/joconde.zip">
+          joconde csv
+        </a><br /> <br />
         <h6>Formats d’import </h6>
         Les formats de données pris en charge sont les suivants&nbsp;: <br />
         <ul>
