3adb52fef4b9938a4158c4caa4be23978e819c63
goffle <se.legoff@gmail.com>
2019-07-22 12:58:07 +0200

    put back

--- apps/production/src/server/index.js
+++ apps/production/src/server/index.js
@@ -12,21 +12,13 @@ function forceHttps(res, req, next) {
   next();
 }
 
-function forceHttp(res, req, next) {
-  const isProdDomain = (req.get("Host") || "").match(/production\.pop\.culture\.gouv\.fr/);
-  if (req.secure && req.get("x-forwarded-proto") === "https" && isProdDomain) {
-    return res.redirect(302, "http://production.pop.culture.gouv.fr" + req.url);
-  }
-  next();
-}
-
 console.log("START", new Date());
 
 const app = express();
-app.set("trust proxy", true);
+app.set('trust proxy', true);
 const port = 8081;
-app.use(forceHttp);
-// app.use(hsts({ maxAge: 31536000, includeSubDomains: true, preload: true }));
+app.use(forceHttps);
+app.use(hsts({ maxAge: 31536000, includeSubDomains: true, preload: true }));
 app.use(express.static(path.join(__dirname, "/../../build")));
 app.route("*").all((req, res) => {
   res.sendFile(path.join(__dirname, "/../../build/index.html"));
