5ed30e31acdaafd2a5bb097a263199f9fb665fbf
goffle <se.legoff@gmail.com>
2019-07-22 12:29:12 +0200

    force http

--- apps/production/src/server/index.js
+++ apps/production/src/server/index.js
@@ -13,8 +13,6 @@ function forceHttps(res, req, next) {
 }
 
 function forceHttp(res, req, next) {
-  console.log(req.get("Host"));
-  console.log(req.hostname);
   const isProdDomain = (req.get("Host") || "").match(/production\.pop\.culture\.gouv\.fr/);
   if (!req.secure && req.get("x-forwarded-proto") === "https" && isProdDomain) {
     return res.redirect(302, "http://production.pop.culture.gouv.fr" + req.url);
@@ -28,7 +26,7 @@ const app = express();
 app.set("trust proxy", true);
 const port = 8081;
 app.use(forceHttp);
-app.use(hsts({ maxAge: 31536000, includeSubDomains: true, preload: true }));
+// app.use(hsts({ maxAge: 31536000, includeSubDomains: true, preload: true }));
 app.use(express.static(path.join(__dirname, "/../../build")));
 app.route("*").all((req, res) => {
   res.sendFile(path.join(__dirname, "/../../build/index.html"));
