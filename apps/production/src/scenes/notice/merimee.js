933753e4f466492d603db387814975e147052c00
bmerciersoprasteria <54849615+bmerciersoprasteria@users.noreply.github.com>
2019-11-22 15:59:10 +0100

    M34585 Producteur État non modifiable (#424)

--- apps/production/src/scenes/notice/merimee.js
+++ apps/production/src/scenes/notice/merimee.js
@@ -59,7 +59,7 @@ class Notice extends React.Component {
     if (this.props.group === "mh") {
       return (
         ["producteur", "administrateur"].includes(this.props.role) &&
-        ["Monuments Historiques", "Architecture", "Etat", "Autre"].includes(notice.PRODUCTEUR)
+        ["Monuments Historiques", "Architecture", "État", "Autre"].includes(notice.PRODUCTEUR)
       );
     }
 
