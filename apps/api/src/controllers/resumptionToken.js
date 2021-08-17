2da9992b846638fe87edafd5eafd9bd334b21519
Johan Crépin <johan.crepin@open-groupe.com>
2021-08-17 17:14:33 +0200

    api - Update dependencies for NodeJS 14 compatibility
    api - Update dependencies for NodeJS 14 compatibility

--- apps/api/src/controllers/resumptionToken.js
+++ apps/api/src/controllers/resumptionToken.js
@@ -1,28 +1,28 @@
 const resumptionToken = require("../models/resumptionToken");
-let moment = require('moment-timezone')
-let uuid = require('uuid/v4')
+const moment = require('moment-timezone')
+const { v4: uuid } = require('uuid')
 
 
-async function getResumptionToken(resumptoken){
-    try{
+async function getResumptionToken(resumptoken) {
+    try {
         const token = await resumptionToken.find({ TOKEN: resumptoken })
         if (token) {
             return token
-        }else{
+        } else {
             return res.status(404).send({ success: false, msg: "token introuvable." })
         }
-    }catch (e) {
+    } catch (e) {
         capture(e);
         res.status(500).send({ success: false, msg: JSON.stringify(e) })
     }
 }
 
-async function createResumptionToken(nbPage,listSize,queryContent){
-    try{
+async function createResumptionToken(nbPage, listSize, queryContent) {
+    try {
         let expDate = new Date()
         let resumpObj = {
             TOKEN: uuid(),
-            DEXP: moment(expDate.setDate(expDate.getDate()+1)).format('YYYY-MM-DDTHH:mm:ss'),
+            DEXP: moment(expDate.setDate(expDate.getDate() + 1)).format('YYYY-MM-DDTHH:mm:ss'),
             CURSOR: nbPage + 1,
             FROM: queryContent.from,
             UNTIL: queryContent.until,
@@ -35,27 +35,28 @@ async function createResumptionToken(nbPage,listSize,queryContent){
         promises.push(obj.save())
         await Promise.all(promises)
         return resumpObj
-    }catch (e) {
-    capture(e);
-    res.status(500).send({ success: false, msg: JSON.stringify(e) })}
+    } catch (e) {
+        capture(e);
+        res.status(500).send({ success: false, msg: JSON.stringify(e) })
+    }
 }
 
 // Update a resumptionToken.
 async function updateResumptionToken(token, updatedObj) {
     return new Promise((resolve, reject) => {
-         resumptionToken.findOneAndUpdate({ token }, updatedObj, { upsert: true, new: true, useFindAndModify: false}, (err, doc) => {
-        if (err) {
-          reject(err);
-          return;
-        }
-        resolve(doc);
-      });
+        resumptionToken.findOneAndUpdate({ token }, updatedObj, { upsert: true, new: true, useFindAndModify: false }, (err, doc) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve(doc);
+        });
     });
-  }
+}
 
-  // delete a resumptionToken.
+// delete a resumptionToken.
 async function deleteResumptionToken(token) {
-        await resumptionToken.deleteOne({ TOKEN: token })
+    await resumptionToken.deleteOne({ TOKEN: token })
 }
 
 module.exports = {
