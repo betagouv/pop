ca3a895d3a1baa512a97d2d86945b77d78099ec3
Lempereur Rodrigue <rodrigue.lempereur@open-groupe.com>
2021-09-14 08:40:38 +0200

    M39010 - Correction scripts de mise à jour des champs

--- apps/api/src/scripts/script_M39010_string_to_array_Memoire.js
+++ apps/api/src/scripts/script_M39010_string_to_array_Memoire.js
@@ -1,59 +1,59 @@
 var notices = db.memoire.find().noCursorTimeout();
 var noticeCount = db.memoire.count();
-notices.forEach(function( aRow ){
-	var ref = aRow.REF;
+notices.forEach(function( notice ){
+	var ref = notice.REF;
 	
 	//Transformation des champ string to array
 	//REG
-	var arrayPays = (!Array.isArray(notice.PAYS)) ? notice.PAYS.split(';').map((element) => { return element.trim() !== "" }) : notice.PAYS;
+	var arrayPays = (!Array.isArray(notice.PAYS)) ? notice.PAYS.split(';').map((element) => { return  element.trim() } ).filter((element) => element !== "" ) : notice.PAYS;
 
 	//REG
-	var arrayReg = (!Array.isArray(notice.REG)) ? notice.REG.split(';').map((element) => { return element.trim() !== "" }) : notice.REG;
+	var arrayReg = (!Array.isArray(notice.REG)) ? notice.REG.split(';').map((element) => { return  element.trim() } ).filter((element) => element !== "" ) : notice.REG;
  
 	//DPT
-	var arrayDpt = (!Array.isArray(notice.DPT)) ? notice.DPT.split(';').map((element) => { return element.trim() !== "" }) : notice.DPT;
+	var arrayDpt = (!Array.isArray(notice.DPT)) ? notice.DPT.split(';').map((element) => { return  element.trim() } ).filter((element) => element !== "" ) : notice.DPT;
  
 	//COM
-	var arrayCom = (!Array.isArray(notice.COM)) ? notice.COM.split(';').map((element) => { return element.trim() !== "" }) : notice.COM;
+	var arrayCom = (!Array.isArray(notice.COM)) ? notice.COM.split(';').map((element) => { return  element.trim() } ).filter((element) => element !== "" ) : notice.COM;
  
 	//INSEE
-	var arrayInsee = (!Array.isArray(notice.INSEE)) ? notice.INSEE.split(';').map((element) => { return element.trim() !== "" }) : notice.INSEE;
+	var arrayInsee = (!Array.isArray(notice.INSEE)) ? notice.INSEE.split(';').map((element) => { return  element.trim() } ).filter((element) => element !== "" ) : notice.INSEE;
  
 	//MCL
-	var arrayMcl = (!Array.isArray(notice.MCL)) ? notice.MCL.split(';').map((element) => { return element.trim() !== "" }) : notice.MCL;
+	var arrayMcl = (!Array.isArray(notice.MCL)) ? notice.MCL.split(';').map((element) => { return  element.trim() } ).filter((element) => element !== "" ) : notice.MCL;
 
 	//AUTOEU
-	var arrayAutoeu = (!Array.isArray(notice.AUTOEU)) ? notice.AUTOEU.split(';').map((element) => { return element.trim() !== "" }) : notice.AUTOEU;
+	var arrayAutoeu = (!Array.isArray(notice.AUTOEU)) ? notice.AUTOEU.split(';').map((element) => { return  element.trim() } ) : notice.AUTOEU;
  
 	//AUTR
-	var arrayAutr = (!Array.isArray(notice.AUTR)) ? notice.AUTR.split(';').map((element) => { return element.trim() !== "" }) : notice.AUTR;
+	var arrayAutr = (notice.AUTR) ? (!Array.isArray(notice.AUTR)) ? notice.AUTR.split(';').map((element) => { return  element.trim() } ).filter((element) => element !== "" ) : notice.AUTR : [];
  
 	//SCLE
-	var arrayScle = (!Array.isArray(notice.SCLE)) ? notice.SCLE.split(';').map((element) => { return element.trim() !== "" }) : notice.SCLE;
+	var arrayScle = (!Array.isArray(notice.SCLE)) ? notice.SCLE.split(';').map((element) => { return  element.trim() } ).filter((element) => element !== "" ) : notice.SCLE;
  
 	//DATOEU
-	var arrayDatoeu = (!Array.isArray(notice.DATOEU)) ? notice.DATOEU.split(';').map((element) => { return element.trim() !== "" }) : notice.DATOEU;
+	var arrayDatoeu = (!Array.isArray(notice.DATOEU)) ? notice.DATOEU.split(';').map((element) => { return  element.trim() } ).filter((element) => element !== "" ) : notice.DATOEU;
  
 	//DOM
-	var arrayDom = (!Array.isArray(notice.DOM)) ? notice.DOM.split(';').map((element) => { return element.trim() !== "" }) : notice.DOM;
+	var arrayDom = (!Array.isArray(notice.DOM)) ? notice.DOM.split(';').map((element) => { return  element.trim() } ).filter((element) => element !== "" ) : notice.DOM;
  
 	//MCPER
-	var arrayMcper = (!Array.isArray(notice.MCPER)) ? notice.MCPER.split(';').map((element) => { return element.trim() !== "" }) : notice.MCPER;
+	var arrayMcper = (!Array.isArray(notice.MCPER)) ? notice.MCPER.split(';').map((element) => { return  element.trim() } ).filter((element) => element !== "" ) : notice.MCPER;
  
 	//AUTG
-	var arrayAutg = (!Array.isArray(notice.AUTG)) ? notice.AUTG.split(';').map((element) => { return element.trim() !== "" }) : notice.AUTG;
+	var arrayAutg = (!Array.isArray(notice.AUTG)) ? notice.AUTG.split(';').map((element) => { return  element.trim() } ).filter((element) => element !== "" ) : notice.AUTG;
  
 	//AUTOR
-	var arrayAutor = (!Array.isArray(notice.AUTOR)) ? notice.AUTOR.split(';').map((element) => { return element.trim() !== "" }) : notice.AUTOR;
+	var arrayAutor = (!Array.isArray(notice.AUTOR)) ? notice.AUTOR.split(';').map((element) => { return  element.trim() } ).filter((element) => element !== "" ) : notice.AUTOR;
  
 	//COTECOR
-	var arrayCotecor = (!Array.isArray(notice.COTECOR)) ? notice.COTECOR.split(';').map((element) => { return element.trim() !== "" }) : notice.COTECOR;
+	var arrayCotecor = (!Array.isArray(notice.COTECOR)) ? notice.COTECOR.split(';').map((element) => { return  element.trim() } ).filter((element) => element !== "" ) : notice.COTECOR;
  
 	//AUTTI
-	var arrayAutti = (!Array.isArray(notice.AUTTI)) ? notice.AUTTI.split(';').map((element) => { return element.trim() !== "" }) : notice.AUTTI;
+	var arrayAutti = (!Array.isArray(notice.AUTTI)) ? notice.AUTTI.split(';').map((element) => { return  element.trim() } ).filter((element) => element !== "" ) : notice.AUTTI;
  
 	//AUT
-	var arrayAut = (!Array.isArray(notice.AUT)) ? notice.AUT.split(';').map((element) => { return element.trim() !== "" }) : notice.AUT;
+	var arrayAut = (notice.AUT) ?  (!Array.isArray(notice.AUT)) ? notice.AUT.split(';').map((element) => { return  element.trim() } ).filter((element) => element !== "" ) : notice.AUT : [];
  
 	//Update field REG, DPT, DPT_Lettre, COM, WCOM, INSEE, COM2, INSEE2, ATEL
 	db.memoire.update(
--- apps/api/src/scripts/script_M39010_string_to_array_Merimee.js
+++ apps/api/src/scripts/script_M39010_string_to_array_Merimee.js
@@ -1,26 +1,26 @@
 var notices = db.merimee.find().noCursorTimeout();
 var noticeCount = db.merimee.count();
-notices.forEach(function( aRow ){
-	var ref = aRow.REF;
+notices.forEach(function( notice ){
+	var ref = notice.REF;
 	
 	//Transformation des champ string to array
 	//REG
-	var arrayReg = (!Array.isArray(notice.REG)) ? notice.REG.split(';').map((element) => { return element.trim() !== "" }) : notice.REG;
+	var arrayReg = (!Array.isArray(notice.REG)) ? notice.REG.split(';').map((element) => { return  element.trim() } ).filter((element) => element !== "" ) : notice.REG;
  
 	//DPT
-	var arrayDpt = (!Array.isArray(notice.DPT)) ? notice.DPT.split(';').map((element) => { return element.trim() !== "" }) : notice.DPT;
+	var arrayDpt = (!Array.isArray(notice.DPT)) ? notice.DPT.split(';').map((element) => { return  element.trim() } ).filter((element) => element !== "" ) : notice.DPT;
  
 	//DPT_Lettre
-	var arrayDpt_Lettre = (!Array.isArray(notice.DPT_Lettre)) ? notice.DPT_Lettre.split(';').map((element) => { return element.trim() !== "" }) : notice.DPT_Lettre;
+	var arrayDpt_Lettre = (notice.DPT_Lettre) ? (!Array.isArray(notice.DPT_Lettre)) ? notice.DPT_Lettre.split(';').map((element) => { return  element.trim() } ).filter((element) => element !== "" ) : notice.DPT_Lettre : [];
  
 	//COM
-	var arrayCom = (!Array.isArray(notice.COM)) ? notice.COM.split(';').map((element) => { return element.trim() !== "" }) : notice.COM;
+	var arrayCom = (!Array.isArray(notice.COM)) ? notice.COM.split(';').map((element) => { return  element.trim() } ).filter((element) => element !== "" ) : notice.COM;
  
 	//WCOM
-	var arrayWcom = (!Array.isArray(notice.WCOM)) ? notice.WCOM.split(';').map((element) => { return element.trim() !== "" }) : notice.WCOM;
+	var arrayWcom = (!Array.isArray(notice.WCOM)) ? notice.WCOM.split(';').map((element) => { return  element.trim() } ).filter((element) => element !== "" ) : notice.WCOM;
  
 	//INSEE
-	var arrayInsee = (!Array.isArray(notice.INSEE)) ? notice.INSEE.split(';').map((element) => { return element.trim() !== "" }) : notice.INSEE;
+	var arrayInsee = (!Array.isArray(notice.INSEE)) ? notice.INSEE.split(';').map((element) => { return  element.trim() } ).filter((element) => element !== "" ) : notice.INSEE;
 
 	//Update fields REG, DPT, DPT_Lettre, COM, WCOM, INSEE
 	db.merimee.update(
