25d334372baee80df4f35ae188eb5d16ab5daca7
Lempereur Rodrigue <rodrigue.lempereur@open-groupe.com>
2021-12-24 00:47:52 +0100

    Mise en place message manitenance sur l'application production

--- apps/production/webpack.dev.config.js
+++ apps/production/webpack.dev.config.js
@@ -17,7 +17,8 @@ module.exports = env => {
     }),
     new webpack.DefinePlugin({
       "process.env": {
-        NODE_ENV: JSON.stringify("dev")
+        NODE_ENV: JSON.stringify("dev"),
+        MAINTENANCE: false
       }
     })
   ];
