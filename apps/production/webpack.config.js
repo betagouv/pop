958e913abe209e2dffc9be64865d0f41aca3cc52
Lempereur Rodrigue <rodrigue.lempereur@open-groupe.com>
2021-12-27 09:05:42 +0100

    désactivation maintenance app production

--- apps/production/webpack.config.js
+++ apps/production/webpack.config.js
@@ -29,7 +29,7 @@ module.exports = env => {
     new webpack.DefinePlugin({
       "process.env": {
         NODE_ENV: JSON.stringify(mode),
-        MAINTENANCE: true
+        MAINTENANCE: false
       }
     }),
     new UglifyJsPlugin({
