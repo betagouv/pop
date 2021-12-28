e088ecdc1efce8682419848ac9203dc7b1a59edc
Lempereur Rodrigue <rodrigue.lempereur@open-groupe.com>
2021-12-28 09:12:53 +0100

    M39257 - Correction scripts reprise des éléments pour champ mémoire

--- apps/api/src/scripts/script_M39257_maj_url_image_absente_merimee.js
+++ apps/api/src/scripts/script_M39257_maj_url_image_absente_merimee.js
@@ -1,28 +1,26 @@
 notices = db.merimee.find( { MEMOIRE: { $gt: { $size: 0 }  } } );
 nbreNotices = notices.count();
 
-let nbrN = 0
-
 print(nbreNotices);
  
 notices.forEach(notice => {
     let arrayMemoire = notice.MEMOIRE.map((element) => {
-        var noticeMemoire = db.memoire.findOne({ REF: element.ref });
-        let memoire = {};
-        if(noticeMemoire){
-            memoire._id = noticeMemoire._id;
-            memoire.ref = noticeMemoire.REF;
-            memoire.url = noticeMemoire.IMG;
-            memoire.copy = noticeMemoire.COPY;
-            memoire.name = noticeMemoire.LEG;
+        if(element){
+            var noticeMemoire = db.memoire.findOne({ REF: element.ref });
+            let memoire = {};
+            if(noticeMemoire){
+                memoire._id = noticeMemoire._id;
+                memoire.ref = noticeMemoire.REF;
+                memoire.url = noticeMemoire.IMG;
+                memoire.copy = noticeMemoire.COPY;
+                memoire.name = noticeMemoire.LEG;
 
-            nbrN++
-            print(nbrN + " notices");
+                return memoire;
+            }
+        }  
+    }).filter(val => val);
 
-            return memoire;
-        }
-        
-    });
+    print(nbreNotices-- + " notices restantes");
 
     db.merimee.update(
         { REF : notice.REF },
