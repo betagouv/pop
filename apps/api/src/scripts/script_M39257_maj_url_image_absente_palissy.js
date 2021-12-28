e3f16bf5fcf3766e4e7b4f509928fe10e3b2578b
Lempereur Rodrigue <rodrigue.lempereur@open-groupe.com>
2021-12-28 09:12:53 +0100

    M39257 - Correction scripts reprise des éléments pour champ mémoire

--- apps/api/src/scripts/script_M39257_maj_url_image_absente_palissy.js
+++ apps/api/src/scripts/script_M39257_maj_url_image_absente_palissy.js
@@ -1,31 +1,26 @@
 notices = db.palissy.find( { MEMOIRE: { $gt: { $size: 0 }  } } );
 nbreNotices = notices.count();
 
-let nbrN = 0
-
 print(nbreNotices);
  
 notices.forEach(notice => {
     let arrayMemoire = notice.MEMOIRE.map((element) => {
         if(element){
-        var noticeMemoire = db.memoire.findOne({ REF: element.ref });
-        let memoire = {};
-        if(noticeMemoire){
-            memoire._id = noticeMemoire._id;
-            memoire.ref = noticeMemoire.REF;
-            memoire.url = noticeMemoire.IMG;
-            memoire.copy = noticeMemoire.COPY;
-            memoire.name = noticeMemoire.LEG;
+            var noticeMemoire = db.memoire.findOne({ REF: element.ref });
+            let memoire = {};
+            if(noticeMemoire){
+                memoire._id = noticeMemoire._id;
+                memoire.ref = noticeMemoire.REF;
+                memoire.url = noticeMemoire.IMG;
+                memoire.copy = noticeMemoire.COPY;
+                memoire.name = noticeMemoire.LEG;
 
-            nbrN++
-            print(nbrN + " notices");
-            print(notice.REF);
+                return memoire;
+            }
+        }  
+    }).filter(val => val);
 
-            return memoire;
-        }
-    }
-        
-    });
+    print(nbreNotices-- + " notices restantes");
 
     db.palissy.update(
         { REF : notice.REF },
