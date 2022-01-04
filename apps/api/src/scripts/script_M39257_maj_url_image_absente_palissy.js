d86a4e7fe63331a562cbf2f5257ad9b3771eb76e
Lempereur Rodrigue <rodrigue.lempereur@open-groupe.com>
2022-01-04 09:49:56 +0100

    M39257 - Correction scripts reprise mérimée et palissy, reprise à partir du champ LBASE Mémoire

--- apps/api/src/scripts/script_M39257_maj_url_image_absente_palissy.js
+++ apps/api/src/scripts/script_M39257_maj_url_image_absente_palissy.js
@@ -1,33 +1,40 @@
-notices = db.palissy.find( { MEMOIRE: { $gt: { $size: 0 }  } } );
+notices = db.palissy.find( { MEMOIRE: { $eq: [] } } );
 nbreNotices = notices.count();
+let countUpdate = 0;
 
 print(nbreNotices);
- 
-notices.forEach(notice => {
-    let arrayMemoire = notice.MEMOIRE.map((element) => {
-        if(element){
-            var noticeMemoire = db.memoire.findOne({ REF: element.ref });
-            let memoire = {};
-            if(noticeMemoire){
-                memoire._id = noticeMemoire._id;
-                memoire.ref = noticeMemoire.REF;
-                memoire.url = noticeMemoire.IMG;
-                memoire.copy = noticeMemoire.COPY;
-                memoire.name = noticeMemoire.LEG;
 
-                return memoire;
-            }
-        }  
+notices.forEach(notice => {
+    // On récupère les notices MEMOIRE liées
+    const noticesMemoire = db.memoire.find( { LBASE: { $elemMatch: { $eq: notice.REF }  } } );
+
+    // Construction du tableau MEMOIRE
+    let arrayMemoire = noticesMemoire.map((noticeMemoire) => {
+        if(noticeMemoire){
+            let memoire = {};
+            memoire._id = noticeMemoire._id;
+            memoire.ref = noticeMemoire.REF;
+            memoire.url = noticeMemoire.IMG;
+            memoire.copy = noticeMemoire.COPY;
+            memoire.name = noticeMemoire.LEG;
+
+            return memoire;
+        }
     }).filter(val => val);
 
-    print(nbreNotices-- + " notices restantes");
-
-    db.palissy.update(
-        { REF : notice.REF },
-        {
-            $set : {
-                MEMOIRE : arrayMemoire
+    if(arrayMemoire.length > 0){
+        db.palissy.update(
+            { REF : notice.REF },
+            {
+                $set : {
+                    MEMOIRE : arrayMemoire
+                }
             }
-        }
-    )
+        )
+
+        countUpdate++;
+    }   
 });
+
+
+print(countUpdate + " notices mise à jour");
