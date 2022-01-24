4c912f4a3c9aa6a581ce99d1665dc9f1e6edafa4
Lempereur Rodrigue <rodrigue.lempereur@open-groupe.com>
2022-01-24 11:38:58 +0100

    M36907 - Modification configuration suite changement de compte Sentry

--- apps/diffusion/src/services/sentry.js
+++ apps/diffusion/src/services/sentry.js
@@ -1,6 +1,7 @@
 import * as Sentry from "@sentry/browser";
 
-const SENTRY_PUBLIC_DSN = "https://9cca185065d74dbd9e05987036f2d16d@sentry.data.gouv.fr/21";
+// const SENTRY_PUBLIC_DSN = "https://9cca185065d74dbd9e05987036f2d16d@sentry.data.gouv.fr/21";
+const SENTRY_PUBLIC_DSN = "https://e38c3de154d443fa86bcbcc815a3c8ea@o1104995.ingest.sentry.io/6164479";
 
 if (typeof window !== "undefined") {
   Sentry.init({ dsn: SENTRY_PUBLIC_DSN });
