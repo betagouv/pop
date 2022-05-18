69a8701e43877a4d0168f52de5a5aad72f45318f
Lempereur Rodrigue <rodrigue.lempereur@open-groupe.com>
2022-05-18 17:34:41 +0200

    M42894 - Modification version react-ES et ajout des libellés pour la pagination

--- apps/production/src/scenes/search/components/utils.js
+++ apps/production/src/scenes/search/components/utils.js
@@ -219,8 +219,14 @@ function toFrenchRegex(text) {
 }
 
 function pagination(total, itemsPerPage, page, setPage) {
+  const titles = { 
+    previousPage: "Page précédente",
+    nextPage: "Page suivante",
+    firstPage: "Première page",
+    lastPage: "Dernière page"
+  }
   const pagination = (
-    <Pagination onChange={p => setPage(p)} total={total} itemsPerPage={itemsPerPage} page={page} />
+    <Pagination onChange={p => setPage(p)} total={total} itemsPerPage={itemsPerPage} page={page} toolTipsBtn={titles}/>
   );
   if (page === 1000) {
     return (
