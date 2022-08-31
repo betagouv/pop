68d85403ee374fb6e4f1488d38af27f25f5feefc
Lempereur Rodrigue <rodrigue.lempereur@open-groupe.com>
2022-08-31 15:31:28 +0200

    Production - Adaptation de la recherche avancée

--- apps/production/src/scenes/search/components/SearchButton.js
+++ apps/production/src/scenes/search/components/SearchButton.js
@@ -1,11 +1,11 @@
-import { Button } from "reactstrap";
 import React from "react";
 import SearchIcon from "../../../assets/icon-search.png";
 
-export default function SearchButton() {
+export default ({title, icon, className, onClick}) => {
+  const children = icon ? <img src={SearchIcon} alt={title} /> : title;
   return (
-    <Button color="primary">
-      <img src={SearchIcon} alt="rechercher" />
-    </Button>
+    <button className={className} title={title} onClick={onClick}>
+      {children}
+    </button>
   );
 }
