00182cff1ad56883f4b82e59e3018b8a90c802fe
El Hacen BOULKHEIR <EBO21797@open-groupe.com>
2022-10-21 17:13:55 +0200

    Revenir à l'état initial

--- apps/production/src/scenes/search/simple/Museo.js
+++ apps/production/src/scenes/search/simple/Museo.js
@@ -70,7 +70,7 @@ export default function render(props) {
               id="cate"
               initialValue={initialValues.get("cate")}
               fields={["CATEG.keyword"]}
-              title="Musées liées à la base Joconde"
+              title="Catégorie du musée"
             />
           </Col>
           <Col xs="9">
