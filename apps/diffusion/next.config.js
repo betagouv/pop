b739c3202e48f31d5e37696baf9bf9be5b4981e9
Lempereur Rodrigue <rodrigue.lempereur@open-groupe.com>
2023-01-25 16:49:42 +0100

    M43944 - Configuration PWA

--- apps/diffusion/next.config.js
+++ apps/diffusion/next.config.js
@@ -1,9 +1,51 @@
-const withPwa = require("next-pwa");
 const Dotenv = require('dotenv-webpack');
 
-module.exports = withPwa({
-   pwa: {
-    dest: 'public'
+const withPWA = require('next-pwa')({
+  dest: "public",
+  disable: "development" !== process.env.NODE_ENV,
+  swSrc: 'service-worker.js'
+});
+
+module.exports = withPWA({
+  webpack: (config, options) => {
+    // Unshift polyfills in main entrypoint.
+    // Source: https://github.com/zeit/next.js/issues/2060#issuecomment-385199026
+    const originalEntry = config.entry;
+    config.entry = async () => {
+      const entries = await originalEntry();
+      if (entries["main.js"]) {
+        entries["main.js"].unshift("./src/polyfill.js");
+      }
+      return entries;
+    };
+
+    // Load environment variables from ".env" file.
+    config.plugins.push(new Dotenv());
+
+    // Source: https://github.com/zeit/styled-jsx#nextjs
+    config.module.rules.push({
+      test: /\.css$/,
+      use: [
+        options.defaultLoaders.babel,
+        {
+          loader: require("styled-jsx/webpack").loader,
+          options: {
+            type: "global"
+          }
+        }
+      ]
+    });
+
+    return config;
+  }
+});
+
+/*
+module.exports = withPWA({
+  useFileSystemPublicRoutes: false,
+  distDir: ".next",
+  compiler: {
+    styledComponents: true,
   },
   webpack: (config, options) => {
     // Unshift polyfills in main entrypoint.
@@ -42,7 +84,7 @@ module.exports = withPwa({
       : 'static/service-worker.js',
     runtimeCaching: [
       {
-        urlPattern: /^https?.*/,
+        urlPattern: /^https?.,
         handler: 'NetworkFirst',
         options: {
           cacheName: 'offlineCache',
@@ -62,3 +104,4 @@ module.exports = withPwa({
     ]
   },
 });
+*/
