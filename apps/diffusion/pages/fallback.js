cc3775637b364c94ff50b427c97c93a22638c5ac
Lempereur Rodrigue <rodrigue.lempereur@open-groupe.com>
2023-01-26 18:57:59 +0100

    M43994 - Ajout page par défaut pour offline

--- apps/diffusion/pages/fallback.js
+++ apps/diffusion/pages/fallback.js
@@ -1,10 +1,53 @@
-import React from 'react';
+import React from "react";
+import Layout from "../src/components/Layout";
+import { Container, Row, Col } from "reactstrap";
+import Head from "next/head";
 
 const fallback = () => (
-  <div>
-    <h1>This is fallback page when device is offline </h1>
-    <small>Route will fallback to this page</small>
-  </div>
+  <Layout>
+          <Container fluid>
+            <div className="not-found">
+              <Head>
+                <title>Page introuvable - POP - Plateforme Ouverte du Patrimoine</title>
+              </Head>
+              <div className="not-found-left">
+                <h1>POPSI !</h1>
+                <p>
+                  La page que vous recherchez est introuvable...
+                  <br />
+                  <br />
+                  Ne désespérez pas, nos archéobogues sont sur le coup !<br />
+                  Vous pouvez <a href="/">essayer une nouvelle recherche</a>
+                </p>
+              </div>
+              <div className="not-found-right">
+                <img src="/static/courbet.jpeg" alt="Gustave courbet, le désespéré" />
+                <br />
+                Gustave courbet, le désespéré
+              </div>
+            </div>
+          </Container>
+          <style jsx>{`
+            .not-found {
+              text-align: center;
+              padding-top: 35vh;
+              display: flex;
+              padding: 60px;
+            }
+            .not-found img {
+              width: 400px;
+            }
+            .not-found .not-found-right {
+              flex: 1;
+            }
+            .not-found .not-found-left {
+              padding-left: 25px;
+              padding-right: 25px;
+              padding-top: 50px;
+              flex: 1;
+            }
+          `}</style>
+        </Layout>
 );
 
 export default fallback;
