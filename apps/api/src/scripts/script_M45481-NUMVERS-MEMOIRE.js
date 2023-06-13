b6179e30bd2fa24b6344cb33ecf1af6f0b4103fb
Lempereur Rodrigue <rodrigue.lempereur@open-groupe.com>
2023-06-13 15:12:16 +0200

    M45481 - Ajout du script pour le champ NUMVERS Memoire

---
+++ apps/api/src/scripts/script_M45481-NUMVERS-MEMOIRE.js
@@ -0,0 +1,19 @@
+notices = db.memoire.find( { $and :[{ PRODUCTEUR: { $regex: /MPP/ } }, { REF: { $regex: /^APMH/  } }, { NUMVERS: { $eq: ""  } }, { TYPDOC: { $regex: /.*négatif.*/i  } } ]} ).noCursorTimeout();
+nbreNotices = notices.count();
+
+print(nbreNotices);
+
+notices.forEach(notice => {
+    db.memoire.update(
+        { REF : notice.REF },
+        {
+            $set : {
+                NUMVERS : "J/80/490"
+            }
+        }
+    )
+    nbreNotices--;
+    print(nbreNotices + " notices restantes");
+});
+
+
