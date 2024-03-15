e77ce7931a40d63e2076ecb9e238733c26b85716
Maxime Cattet <m@root.bar>
2024-03-15 15:19:12 +0100

    fix(edition): Fix replace env scripts

--- apps/production/scripts/replace_process_env.sh
+++ apps/production/scripts/replace_process_env.sh
@@ -2,10 +2,12 @@ replace_string() {
 	local input="$1"
 	local replace_by="$2"
 
-	grep -rli "${input}" ./build | xargs -i@ sed -i "s/${input}/${replace_by}/g" @
+	echo "Replacing $input by $replace_by"
+
+	grep -Rl "${input}" ./build | xargs -i@ sed -i "s|$input|$replace_by|g" @
 }
 
-replace_string "OVERRIDE_API_URL" "${API_URL}"
+replace_string "OVERRIDE_API_URL" "$API_URL"
 replace_string "OVERRIDE_POP_URL" "${POP_URL}"
 replace_string "OVERRIDE_BUCKET_URL" "${BUCKET_URL}"
 replace_string "OVERRIDE_PUBLIC_URL" "${PUBLIC_URL}"
