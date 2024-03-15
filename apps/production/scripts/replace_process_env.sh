2b009183000101d67542ce5a2e7a76d6453ab76f
Maxime Cattet <m@root.bar>
2024-03-15 14:35:20 +0100

    chore: Use bash to replace str

---
+++ apps/production/scripts/replace_process_env.sh
@@ -0,0 +1,11 @@
+replace_string() {
+	local input="$1"
+	local replace_by="$2"
+
+	grep -rli "${input}" ./build | xargs -i@ sed -i "s/${input}/${replace_by}/g" @
+}
+
+replace_string "OVERRIDE_API_URL" "${API_URL}"
+replace_string "OVERRIDE_POP_URL" "${POP_URL}"
+replace_string "OVERRIDE_BUCKET_URL" "${BUCKET_URL}"
+replace_string "OVERRIDE_PUBLIC_URL" "${PUBLIC_URL}"
