995c411e98425c6ffb3b3cd7e1952c9e01eb5a99
Maxime Cattet <m@root.bar>
2024-04-09 15:13:13 +0200

    fix: Fix bucket_url

--- apps/diffusion/src/config.js
+++ apps/diffusion/src/config.js
@@ -8,34 +8,7 @@ function deleteLastSlash(url) {
 	return url;
 }
 
-// const configs = {
-// 	dev: {
-// 		//
-// 	},
-// 	staging: {
-// 		//
-// 	},
-// 	prod: {
-// 		//
-// 	},
-// };
-
-// const getConfig = () => {
-// 	if (process.env.NEXT_PUBLIC_OVH) {
-// 		return {
-// 			api_url: deleteLastSlash(process.env.NEXT_PUBLIC_API_URL),
-// 			server_api_url: deleteLastSlash(process.env.API_URL),
-// 			bucket_url: `${process.env.NEXT_PUBLIC_BUCKET_URL}/`,
-// 			pop_url: process.env.NEXT_PUBLIC_POP_URL,
-// 			eurelian: process.env.NEXT_PUBLIC_EURELIAN,
-// 			es_url: `${api_url}/search/`,
-// 			sentryDsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-// 		};
-// 	}
-// };
-
 const api_url = deleteLastSlash(process.env.API_URL);
-const server_api_url = deleteLastSlash(process.env.API_URL);
 const bucket_url = `${process.env.BUCKET_URL}/`;
 const pop_url = process.env.POP_URL;
 const eurelian = process.env.EURELIAN;
@@ -49,7 +22,6 @@ const emailContactMnr =
 
 module.exports = {
 	api_url,
-	server_api_url,
 	es_url,
 	bucket_url,
 	pop_url,
