ed6ebbe8369a8ce3f60d9ebca0fc8b78bb6c7261
Maxime Cattet <m@root.bar>
2024-04-10 16:20:43 +0200

    chore: Try thing to switch between env aws & ovh

--- apps/diffusion/next.config.js
+++ apps/diffusion/next.config.js
@@ -1,23 +1,5 @@
 const Dotenv = require("dotenv-webpack");
 
-const withPWA = require("next-pwa")({
-	dest: "public",
-	disable: "development" === process.env.NODE_ENV,
-	swSrc: "service-worker.js",
-});
-
-const cspHeaders = `
-  default-src 'self' https://www.pop.culture.gouv.fr;
-  script-src 'self' https://cdnjs.cloudflare.com https://www.pop.culture.gouv.fr http://stats.data.gouv.fr http://io1.eulerian.net 'unsafe-eval' 'unsafe-inline' http://uvvw.pop.culture.gouv.fr https://static.hotjar.com https://script.hotjar.com;
-  style-src 'self' https://cdnjs.cloudflare.com 'unsafe-inline' https://www.pop.culture.gouv.fr https://static.hotjar.com;
-  font-src 'self' https://cdnjs.cloudflare.com https://www.pop.culture.gouv.fr data:;
-  connect-src 'self' ${
-		process.env.API_URL ? process.env.API_URL : "http://localhost:3000"
-  } https://cdnjs.cloudflare.com https://s3.eu-west-3.amazonaws.com https://www.pop.culture.gouv.fr https://uvvw.pop.culture.gouv.fr http://stats.data.gouv.fr https://static.hotjar.com https://o1104995.ingest.sentry.io https://script.hotjar.com https://api.mapbox.com https://api.tiles.mapbox.com;
-  img-src 'self' *.amazonaws.com http://stats.data.gouv.fr https://www.pop.culture.gouv.fr;
-  object-src 'self' data:;
-`;
-
 function deleteLastSlash(url) {
 	if (url == null) return;
 
@@ -28,51 +10,40 @@ function deleteLastSlash(url) {
 	return url;
 }
 
-module.exports = {
-	webpack: (config, options) => {
-		// Unshift polyfills in main entrypoint.
-		// Source: https://github.com/zeit/next.js/issues/2060#issuecomment-385199026
-		const originalEntry = config.entry;
-		config.entry = async () => {
-			const entries = await originalEntry();
-			if (entries["main.js"]) {
-				entries["main.js"].unshift("./src/polyfill.js");
-			}
-			return entries;
-		};
-
-		// Load environment variables from ".env" file.
-		if (process.env.OVH == null) {
-			config.plugins.push(new Dotenv());
+const webpackConfig = (config, options) => {
+	// Unshift polyfills in main entrypoint.
+	// Source: https://github.com/zeit/next.js/issues/2060#issuecomment-385199026
+	const originalEntry = config.entry;
+	config.entry = async () => {
+		const entries = await originalEntry();
+		if (entries["main.js"]) {
+			entries["main.js"].unshift("./src/polyfill.js");
 		}
+		return entries;
+	};
 
-		// Source: https://github.com/zeit/styled-jsx#nextjs
-		config.module.rules.push({
-			test: /\.css$/,
-			use: [
-				options.defaultLoaders.babel,
-				{
-					loader: require("styled-jsx/webpack").loader,
-					options: {
-						type: "global",
-					},
+	// Load environment variables from ".env" file.
+	if (process.env.OVH == null) {
+		config.plugins.push(new Dotenv());
+	}
+
+	// Source: https://github.com/zeit/styled-jsx#nextjs
+	config.module.rules.push({
+		test: /\.css$/,
+		use: [
+			options.defaultLoaders.babel,
+			{
+				loader: require("styled-jsx/webpack").loader,
+				options: {
+					type: "global",
 				},
-			],
-		});
+			},
+		],
+	});
 
-		return config;
-	},
-	// async headers() {
-	//   return [
-	//     {
-	//       source: '/(.*)',
-	//       headers: [
-	//         {
-	//           key: 'Content-Security-Policy',
-	//           value: cspHeaders.replace(/\n/g, "")
-	//         },
-	//       ],
-	//     },
-	//   ];
-	// },
+	return config;
+};
+
+module.exports = {
+	webpack: webpackConfig,
 };
--- apps/diffusion/src/config.js
+++ apps/diffusion/src/config.js
@@ -8,28 +8,27 @@ function deleteLastSlash(url) {
 	return url;
 }
 
-const api_url = deleteLastSlash(
-	process.env.API_URL ?? process.env.NEXT_PUBLIC_API_URL,
-);
-const bucket_url = `${
-	process.env.BUCKET_URL ?? process.env.NEXT_PUBLIC_BUCKET_URL
-}/`;
-const pop_url = process.env.POP_URL ?? process.env.NEXT_PUBLIC_POP_URL;
-const eurelian = process.env.EURELIAN ?? process.env.NEXT_PUBLIC_EURELIAN;
-const es_url = `${api_url}/search/`;
-const sentryDsn = process.env.SENTRY_DSN ?? process.env.NEXT_PUBLIC_SENTRY_DSN;
+const config = {};
 
-console.log("api_url", api_url);
+if (process.env.OVH === "true") {
+	config.api_url = deleteLastSlash(process.env.NEXT_PUBLIC_API_URL);
+	config.bucket_url = `${process.env.NEXT_PUBLIC_BUCKET_URL}/`;
+	config.pop_url = process.env.NEXT_PUBLIC_POP_URL;
+	config.eurelian = process.env.NEXT_PUBLIC_EURELIAN;
+	config.sentryDsn = process.env.NEXT_PUBLIC_SENTRY_DSN;
+} else {
+	config.api_url = deleteLastSlash(process.env.API_URL);
+	config.bucket_url = `${process.env.BUCKET_URL}/`;
+	config.pop_url = process.env.POP_URL;
+	config.eurelian = process.env.EURELIAN;
+	config.sentryDsn = process.env.SENTRY_DSN;
+}
 
-const emailContactMnr =
+config.es_url = `${config.api_url}/search/`;
+
+console.log("api_url", config.api_url);
+
+config.emailContactMnr =
 	"isabelle.rouge-ducos@culture.gouv.fr;contact.m2rs@culture.gouv.fr";
 
-module.exports = {
-	api_url,
-	es_url,
-	bucket_url,
-	pop_url,
-	emailContactMnr,
-	eurelian,
-	sentryDsn,
-};
+module.exports = config;
